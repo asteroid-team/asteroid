3cf2df45512906a20db9a6c7f6a1e3be65f3871e
vitrioil <opm249@gmail.com>
2019-10-20 00:08:28 +0530

    Now video tensors are embeddings instead of raw video frames == Solanki Khush??

--- loader/data_loader.py
+++ loader/data_loader.py
@@ -3,12 +3,13 @@ import torch
 import pandas as pd
 from data import Signal
 from pathlib import Path
+from frames import input_face_embeddings
 from audio_feature_generator import convert_to_spectrogram
 
 class AVDataset(torch.utils.data.Dataset):
     
 
-    def __init__(self, dataset_df_path: Path, video_base_dir: Path, input_df_path: Path, input_audio_size=2):
+    def __init__(self, dataset_df_path: Path, video_base_dir: Path, input_df_path: Path, input_audio_size=2, use_cuda=True):
         self.input_audio_size = input_audio_size
 
         self.dataset_df = pd.read_csv(dataset_df_path.as_posix())
@@ -22,6 +23,8 @@ class AVDataset(torch.utils.data.Dataset):
         self.face_y = self.dataset_df.iloc[:, 4]
 
         self.input_df = pd.read_csv(input_df_path.as_posix())
+
+        self.use_cuda = use_cuda
 
     def __len__(self):
         return len(self.input_df)
@@ -44,9 +47,10 @@ class AVDataset(torch.utils.data.Dataset):
         for i in range(self.input_audio_size):
             spectrogram = convert_to_spectrogram(all_signals[i].get_audio())
             audio_tensors.append(torch.from_numpy(spectrogram))
-
-            #TODO: Convert raw video to face embedding
-            video_tensors.append(torch.from_numpy(all_signals[i].get_video()))
+            raw_frames = all_signals[i].get_video()
+            #NOTE: use_cuda = True, only if VRAM ~ 7+GB, if RAM < 8GB it will not work...
+            embeddings = input_face_embeddings(raw_frames, use_cuda=False)
+            video_tensors.append(torch.stack(embeddings))
 
         # video tensors are expected to be (75,1,1024) (h,w,c)
         # list of video tensors where len(list) == num_person
@@ -58,6 +62,10 @@ class AVDataset(torch.utils.data.Dataset):
         mixed_signal_tensor = torch.Tensor(convert_to_spectrogram(mixed_signal))  #shape  (257,298,2)
         mixed_signal_tensor = torch.transpose(mixed_signal_tensor,0,2) #shape (2,298,257)  , therefore , 2 channels , height = 298 , width = 257	
 
+        if self.use_cuda:
+            audio_tensors = [a.cuda() for a in audio_tensors]
+            video_tensors = [a.cuda() for a in video_tensors]
+            mixed_signal_tensor = mixed_signal_tensor.cuda()
         return audio_tensors, video_tensors, mixed_signal_tensor
 
 
