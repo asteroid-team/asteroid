fd7579c56e353864c82f230c49e97c2f875c555d
vitrioil <opm249@gmail.com>
2019-10-19 23:21:53 +0530

    Argument for file/array, zeros in case no face in a frame and choose only one face in each frame

--- loader/frames.py
+++ loader/frames.py
@@ -1,22 +1,35 @@
 from facenet_pytorch import MTCNN, InceptionResnetV1
 from PIL import Image
+import cv2
+import torch
+import numpy as np
 
-def input_face_embeddings(frames_path):
+def input_face_embeddings(frames, is_path=False):
     mtcnn = MTCNN(keep_all=True)
     resnet = InceptionResnetV1(pretrained='vggface2').eval()
     result = []
-    for path in frames_path:
+    for f in frames:
         embeddings = []
-        frame = Image.open(path)
+        if is_path:
+            frame = Image.open(f)
+        else:
+            frame = Image.fromarray(f.astype("uint8"))
         cropped_tensors = mtcnn(frame)
+        if cropped_tensors is None:
+            #Apparently trimmed video has few frames without any face -_-
+            #Hence, to maintain 75 frames appending zeros...
+            result.append(torch.zeros((1, 512)))
+            continue
         for face in cropped_tensors:
             emb = resnet(face.unsqueeze(0))
             embeddings.append(emb)
-        result.append(embeddings)
+        #Training requires one face per photo, during inference face will be selected
+        #according to the bounding boxes
+        result.append(embeddings[0])
     return result
 
 
 if __name__ == '__main__':
-    res = input_face_embeddings(["a.jpg","b.jpg"])
+    res = input_face_embeddings(["a.jpg","b.jpg"], True)
     print(res[0][0].shape) # 512D
     print("Passed")
