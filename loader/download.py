ba182270055e661bade8a3e44e9c2c018762fa88
vitrioil <opm249@gmail.com>
2019-10-20 22:31:51 +0530

    Lower Resolution Option

--- loader/download.py
+++ loader/download.py
@@ -15,7 +15,7 @@ def download(link, cwd="../../data/train"):
     p = subprocess.Popen(command.format(link, link[-11:]), shell=True, stdout=subprocess.PIPE, cwd=cwd).communicate()
 
 
-def crop(path, start, end):
+def crop(path, start, end, resolution="640x480"):
     command = "ffmpeg -y -i {} -ss {} -t {} -c:v libx264 -crf 18 -preset veryfast -pix_fmt yuv420p -c:a aac -b:a 128k -strict experimental -r 25 {}" 
 
     start_minute, start_second = int(start // 60), int(start % 60)
@@ -30,10 +30,14 @@ def crop(path, start, end):
 
     p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).communicate()
 
+    low_res_filepath = os.path.join(parent, name + "_final.mp4")
+    downsample = f"avconv -i {new_filepath} -s {resolution} {low_res_filepath} -y"
+    p = subprocess.Popen(downsample, shell=True, stdout=subprocess.PIPE).communicate()
 
-def save_video(link, path, start, end):
+
+def save_video(link, path, start, end, resolution):
     download(link)
-    crop(path, start, end)
+    crop(path, start, end, resolution)
 
 
 def main(args):
@@ -47,7 +51,7 @@ def main(args):
 
     link_path = zip(yt_links, paths, start_times, end_times)
     with concurrent.futures.ThreadPoolExecutor(args.jobs) as executor:
-        futures = [executor.submit(save_video, l, p, s, e) for l, p, s, e in link_path]
+        futures = [executor.submit(save_video, l, p, s, e, args.resolution) for l, p, s, e in link_path]
 
         for f in concurrent.futures.as_completed(futures):
             pass
@@ -58,6 +62,7 @@ if __name__ == "__main__":
     parse.add_argument("--jobs", type=int, default=2)
     parse.add_argument("--path", type=str, default="../../data/audio_visual/avspeech_train.csv")
     parse.add_argument("--vid-dir", type=str, default="../../data/train/")
+    parse.add_argument("--resolution", type=str, default="320x200")
     args = parse.parse_args()
     main(args)
     
