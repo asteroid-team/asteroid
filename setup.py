93964890c5c3eaa31b114d047f7615c6a854d6b9
mpariente <pariente.mnl@gmail.com>
2019-10-27 15:49:51 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='asteroid',
+    version='0.0.1',
+    description='Source separation on steroids',
+    author='Manuel Pariente',
+    author_email='manuel.pariente@loria.fr'
+)
