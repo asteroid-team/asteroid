df3d2e66329cde6e2ff989a596bd29a948e2e9b7
mpariente <pariente.mnl@gmail.com>
2020-02-06 14:30:21 +0100

    [tests] Update tests to run snr and sd-sdr

--- tests/losses/si_sdr_test.py
+++ tests/losses/si_sdr_test.py
@@ -3,28 +3,34 @@ import torch
 from torch.testing import assert_allclose
 
 from asteroid.losses import PITLossWrapper
-from asteroid.losses import nosrc_neg_sisdr, pairwise_neg_sisdr, \
-    nonpit_neg_sisdr
+from asteroid.losses import sdr
 
 
 @pytest.mark.parametrize("n_src", [2, 3, 4])
-def test_sisdr(n_src):
+@pytest.mark.parametrize("function_triplet", [
+    [sdr.pairwise_neg_sisdr, sdr.nosrc_neg_sisdr, sdr.nonpit_neg_sisdr],
+    [sdr.pairwise_neg_sdsdr, sdr.nosrc_neg_sdsdr, sdr.nonpit_neg_sdsdr],
+    [sdr.pairwise_neg_snr, sdr.nosrc_neg_snr, sdr.nonpit_neg_snr],
+])
+def test_sisdr(n_src, function_triplet):
+    # Unpack the triplet
+    pairwise, nosrc, nonpit = function_triplet
+    # Fake targets and estimates
     targets = torch.randn(2, n_src, 32000)
     est_targets = torch.randn(2, n_src, 32000)
-    pw_wrapper = PITLossWrapper(pairwise_neg_sisdr, mode='pairwise')
-    wo_src_wrapper = PITLossWrapper(nosrc_neg_sisdr, mode='wo_src')
-    w_src_wrapper = PITLossWrapper(nonpit_neg_sisdr, mode='w_src')
+    # Create the 3 PIT wrappers
+    pw_wrapper = PITLossWrapper(pairwise, mode='pairwise')
+    wo_src_wrapper = PITLossWrapper(nosrc, mode='wo_src')
+    w_src_wrapper = PITLossWrapper(nonpit, mode='w_src')
 
-    pw = pw_wrapper(targets, est_targets)
-    wo_src = wo_src_wrapper(targets, est_targets)
-    w_src = w_src_wrapper(targets, est_targets)
-
+    # Circular tests on value
     assert_allclose(pw_wrapper(targets, est_targets),
                     wo_src_wrapper(targets, est_targets))
-    assert_allclose(w_src_wrapper(targets, est_targets),
-                    wo_src_wrapper(targets, est_targets))
+    assert_allclose(wo_src_wrapper(targets, est_targets),
+                    w_src_wrapper(targets, est_targets))
 
+    # Circular tests on returned estimates
     assert_allclose(pw_wrapper(targets, est_targets, return_est=True)[1],
                     wo_src_wrapper(targets, est_targets, return_est=True)[1])
-    assert_allclose(w_src_wrapper(targets, est_targets, return_est=True)[1],
-                    wo_src_wrapper(targets, est_targets, return_est=True)[1])
+    assert_allclose(wo_src_wrapper(targets, est_targets, return_est=True)[1],
+                    w_src_wrapper(targets, est_targets, return_est=True)[1])
