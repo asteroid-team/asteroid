adcc78fa43c2fbdaccd008cbcb5a134a5d6a97f5
popcornell <cornellsamuele@gmail.com>
2020-02-17 23:16:01 +0100

    added recipe for DPRNN

---
+++ egs/wham/DPRNN/model.py
@@ -0,0 +1,46 @@
+from torch import nn
+
+import asteroid.filterbanks as fb
+from asteroid.masknn import DPRNN
+from asteroid.engine.optimizers import make_optimizer
+
+
+class Model(nn.Module):
+    def __init__(self, encoder, masker, decoder):
+        super().__init__()
+        self.encoder = encoder
+        self.masker = masker
+        self.decoder = decoder
+
+    def forward(self, x):
+        if len(x.shape) == 2:
+            x = x.unsqueeze(1)
+        tf_rep = self.encoder(x)
+        est_masks = self.masker(tf_rep)
+        masked_tf_rep = est_masks * tf_rep.unsqueeze(1)
+        return self.pad_output_to_inp(self.decoder(masked_tf_rep), x)
+
+    @staticmethod
+    def pad_output_to_inp(output, inp):
+        """ Pad first argument to have same size as second argument"""
+        inp_len = inp.size(-1)
+        output_len = output.size(-1)
+        return nn.functional.pad(output, [0, inp_len - output_len])
+
+
+def make_model_and_optimizer(conf):
+    """ Function to define the model and optimizer for a config dictionary.
+    Args:
+        conf: Dictionary containing the output of hierachical argparse.
+    Returns:
+        model, optimizer.
+    The main goal of this function is to make reloading for resuming
+    and evaluation very simple.
+    """
+    # Define building blocks for local model
+    enc, dec = fb.make_enc_dec('free', **conf['filterbank'])
+    masker = DPRNN(**conf['masknet'])
+    model = Model(enc, masker, dec)
+    # Define optimizer of this model
+    optimizer = make_optimizer(model.parameters(), **conf['optim'])
+    return model, optimizer
---
+++ egs/wham/DPRNN/run.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+
+set -e  # Exit on error
+# Main storage directory. You'll need disk space to dump the WHAM mixtures and the wsj0 wav
+# files if you start from sphere files.
+storage_dir=/media/sam/cb915f0e-e440-414c-bb74-df66b311d09d/
+
+# If you start from the sphere files, specify the path to the directory and start from stage 0
+sphere_dir=  # Directory containing sphere files
+# If you already have wsj0 wav files, specify the path to the directory here and start from stage 1
+wsj0_wav_dir=${storage_dir}/wsj0_wav/
+# If you already have the WHAM mixtures, specify the path to the directory here and start from stage 2
+wham_wav_dir=${storage_dir}/2speakers_wham/
+# After running the recipe a first time, you can run it from stage 3 directly to train new models.
+
+# Path to the python you'll use for the experiment. Defaults to the current python
+# You can run ./utils/prepare_python_env.sh to create a suitable python environment, paste the output here.
+#python_path=${storage_dir}/asteroid_conda/miniconda3/bin/python
+python_path=python
+
+# Example usage
+# ./run.sh --stage 3 --tag my_tag --task sep_noisy --id 0,1
+
+# General
+stage=0  # Controls from which stage to start
+tag=""  # Controls the directory name associated to the experiment
+# You can ask for several GPUs using id (passed to CUDA_VISIBLE_DEVICES)
+id=0
+
+# Data
+data_dir=data  # Local data directory (No disk space needed)
+task=sep_clean  # Specify the task here (sep_clean, sep_noisy, enh_single, enh_both)
+sample_rate=8000
+mode=min
+nondefault_src=  # If you want to train a network with 3 output streams for example.
+
+# Training
+batch_size=8
+num_workers=8
+#optimizer=adam
+lr=0.001
+epochs=200
+
+# Evaluation
+eval_use_gpu=1
+
+
+. utils/parse_options.sh
+
+
+if [[ $stage -le  -1 ]]; then
+	echo "Stage -1: Creating python environment to run this"
+	if [[ -x "${python_path}" ]]
+	then
+		echo "The provided python path is executable, don't proceed to installation."
+	else
+	  . utils/prepare_python_env.sh --install_dir $python_path --asteroid_root ../../..
+	  echo "Miniconda3 install can be found at $python_path"
+	  python_path=${python_path}/miniconda3/bin/python
+	  echo -e "\n To use this python version for the next experiments, change"
+	  echo -e "python_path=$python_path at the beginning of the file \n"
+	fi
+fi
+
+
+
+if [[ $stage -le  0 ]]; then
+  echo "Stage 0: Converting sphere files to wav files"
+  . local/convert_sphere2wav.sh --sphere_dir $sphere_dir --wav_dir $wsj0_wav_dir
+fi
+
+
+if [[ $stage -le  1 ]]; then
+	echo "Stage 1: Generating 8k and 16k WHAM dataset"
+  . local/prepare_data.sh --wav_dir $wsj0_wav_dir --out_dir $wham_wav_dir --python_path $python_path
+fi
+
+
+if [[ $stage -le  2 ]]; then
+	# Make json directories with min/max modes and sampling rates
+	echo "Stage 2: Generating json files including wav path and duration"
+	for sr_string in 8 16; do
+		for mode in min max; do
+			tmp_dumpdir=data/wav${sr_string}k/$mode
+			echo "Generating json files in $tmp_dumpdir"
+			[[ ! -d $tmp_dumpdir ]] && mkdir -p $tmp_dumpdir
+			local_wham_dir=$wham_wav_dir/wav${sr_string}k/$mode/
+      $python_path local/preprocess_wham.py --in_dir $local_wham_dir --out_dir $tmp_dumpdir
+    done
+  done
+fi
+
+sr_string=$(($sample_rate/1000))
+suffix=wav${sr_string}k/$mode
+dumpdir=data/$suffix  # directory to put generated json file
+
+train_dir=$dumpdir/tr
+valid_dir=$dumpdir/cv
+test_dir=$dumpdir/tt
+
+# Generate a random ID for the run if no tag is specified
+uuid=$($python_path -c 'import uuid, sys; print(str(uuid.uuid4())[:8])')
+if [[ -z ${tag} ]]; then
+	tag=${task}_${sr_string}k${mode}_${uuid}
+fi
+expdir=exp/train_convtasnet_${tag}
+mkdir -p $expdir && echo $uuid >> $expdir/run_uuid.txt
+echo "Results from the following experiment will be stored in $expdir"
+
+
+if [[ $stage -le 3 ]]; then
+  echo "Stage 3: Training"
+  mkdir -p logs
+  CUDA_VISIBLE_DEVICES=$id $python_path train.py \
+  --train_dir $train_dir \
+  --valid_dir $valid_dir \
+  --task $task \
+  --sample_rate $sample_rate \
+  --lr $lr \
+  --epochs $epochs \
+  --batch_size $batch_size \
+  --num_workers $num_workers \
+  --exp_dir ${expdir}/ | tee logs/train_${tag}.log
+fi
+
+
+if [[ $stage -le 4 ]]; then
+	echo "Stage 4 : Evaluation"
+	CUDA_VISIBLE_DEVICES=$id $python_path eval.py \
+	--task $task \
+	--test_dir $test_dir \
+	--use_gpu $eval_use_gpu \
+	--exp_dir ${expdir}
+fi
