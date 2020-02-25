273da0f23ac32e54388e8773c0f71bbaf329a349
sunits <sunit.surabhi@gmail.com>
2020-02-25 11:18:03 +0100

    Parameterized test case

--- tests/masknn/blocks_test.py
+++ tests/masknn/blocks_test.py
@@ -33,15 +33,13 @@ def test_dprnn(mask_act, out_chan, hop_size):
     out_chan = out_chan if out_chan else in_chan
     assert out.shape == (batch, n_src, out_chan, n_frames)
 
-def test_chimerapp():
+
+@pytest.mark.parametrize("embed_dim", [10, 20, 30])
+def test_chimerapp(embed_dim):
     in_chan, n_src = 512, 2
-    model = blocks.ChimeraPP(in_chan, n_src)
+    model = blocks.ChimeraPP(in_chan, n_src, embedding_dim=embed_dim)
     batch, freq_dim, nframes = 10, in_chan, 10
     inp = torch.randn(batch, in_chan, nframes)
     out = model(inp)
-    assert out[0].shape == (batch, freq_dim*nframes, 20)
+    assert out[0].shape == (batch, freq_dim*nframes, embed_dim)
     assert out[1].shape == (batch, n_src, in_chan, nframes)
-
-    model = blocks.ChimeraPP(in_chan, n_src, embedding_dim=40)
-    out = model(inp)
-    assert out[0].shape == (batch, freq_dim*nframes, 40)
