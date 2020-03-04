a27d27e690dc80e4de5249e749b0a86a61a5eff5
Pariente Manuel <pariente.mnl@gmail.com>
2020-03-04 14:28:01 +0100

    Perfectly invertible STFT (#38)
    * [src] Fix STFT and iSTFT
    
    * [src] Add possibility to compute perfect resynthesis window 
    
    * [tests] Unit test perfect reconstruction with default parameters.
    
    * [tests] Unit test perfect resynthesis window for common window choices

--- asteroid/filterbanks/stft_fb.py
+++ asteroid/filterbanks/stft_fb.py
@@ -36,18 +36,47 @@ class STFTFB(Filterbank):
             self.window = window
         # Create and normalize DFT filters (can be overcomplete)
         filters = np.fft.fft(np.eye(n_filters))
-        filters /= (.5 * np.sqrt(kernel_size * n_filters / self.stride))
+        filters /= (0.5 * np.sqrt(kernel_size * n_filters / self.stride))
+
         # Keep only the windowed centered part to save computation.
         lpad = int((n_filters - kernel_size) // 2)
         rpad = int(n_filters - kernel_size - lpad)
         indexes = list(range(lpad, n_filters - rpad))
         filters = np.vstack([np.real(filters[:self.cutoff, indexes]),
                              np.imag(filters[:self.cutoff, indexes])])
+
         filters[0, :] /= np.sqrt(2)
-        filters[-1, :] /= np.sqrt(2)
+        filters[n_filters // 2, :] /= np.sqrt(2)
         filters = torch.from_numpy(filters * self.window).unsqueeze(1).float()
         self.register_buffer('_filters', filters)
 
     @property
     def filters(self):
         return self._filters
+
+
+def perfect_synthesis_window(analysis_window, hop_size):
+    """ Computes a window for perfect synthesis given an analysis window and
+        a hop size.
+
+    Args:
+        analysis_window (np.array): Analysis window of the transform.
+        hop_size (int): Hop size in number of samples.
+
+    Returns:
+        np.array : the synthesis window to use for perfectly inverting the STFT.
+    """
+    win_size = len(analysis_window)
+    den = np.zeros_like(analysis_window)
+
+    loop_on = (win_size - 1) // hop_size
+    for win_idx in range(-loop_on, loop_on + 1):
+        shifted = np.roll(analysis_window ** 2, win_idx * hop_size)
+        if win_idx < 0:
+            shifted[win_idx * hop_size:] = 0
+        elif win_idx > 0:
+            shifted[:win_idx * hop_size] = 0
+        den += shifted
+    den = np.where(den != 0., den, np.finfo(den.dtype).tiny)
+    correction = int(0.5 * len(analysis_window) / hop_size)
+    return correction * analysis_window / den
--- tests/filterbanks/stft_test.py
+++ tests/filterbanks/stft_test.py
@@ -2,16 +2,18 @@ import pytest
 import torch
 from torch import testing
 import numpy as np
+from scipy.signal import get_window
 
 from asteroid.filterbanks import Encoder, Decoder, STFTFB
 from asteroid.filterbanks import make_enc_dec
+from asteroid.filterbanks.stft_fb import perfect_synthesis_window
 
 
 def fb_config_list():
     keys = ['n_filters', 'kernel_size', 'stride']
     param_list = [
-        [512, 256, 128],  # Usual STFT, 50% overlap
-        [512, 256, 64],  # Usual STFT, 25% overlap
+        [256, 256, 128],  # Usual STFT, 50% overlap
+        [256, 256, 64],  # Usual STFT, 25% overlap
         [512, 32, 16],  # Overcomplete STFT, 50% overlap
     ]
     return [dict(zip(keys, values)) for values in param_list]
@@ -40,7 +42,6 @@ def test_stft_windows(fb_config):
 
 @pytest.mark.parametrize("fb_config", fb_config_list())
 def test_filter_shape(fb_config):
-    # for fb_config in fb_config_list:
     # Instantiate STFT
     fb = STFTFB(**fb_config)
     # Check filter shape.
@@ -49,38 +50,32 @@ def test_filter_shape(fb_config):
 
 
 @pytest.mark.parametrize("fb_config", fb_config_list())
-def test_istft(fb_config):
-    """ Without dividing by the overlap-added window, the STFT iSTFT cannot
-    pass the unit test. Uncomment the plot to see the perfect resynthesis."""
+def test_perfect_istft_default_parameters(fb_config):
+    """ Unit test perfect reconstruction with default values. """
     kernel_size = fb_config['kernel_size']
     enc, dec = make_enc_dec('stft', **fb_config)
     inp_wav = torch.randn(2, 1, 32000)
     out_wav = dec(enc(inp_wav))[:, :, kernel_size: -kernel_size]
     inp_test = inp_wav[:, :, kernel_size: -kernel_size]
-    # testing.assert_allclose(inp_test,
-    #                         out_wav,
-    #                         rtol=1e-7, atol=1e-3)
-    # import matplotlib.pyplot as plt
-    # plt.plot(inp_test.data.numpy()[0, 0], 'b')
-    # plt.plot(out_wav.data.numpy()[0, 0], 'r')
-    # plt.show()
+    testing.assert_allclose(inp_test, out_wav)
 
 
-@pytest.mark.parametrize('kernel_size', [256])
-@pytest.mark.parametrize('stride', [128, 64])
-def test_ola(kernel_size, stride):
-    """ Unit-test the perfect OLA for boxcar weighted DFT filters."""
-    fb_config = {
-            'n_filters': 2 * kernel_size,
-            'kernel_size': kernel_size,
-            'stride': stride
-        }
-    # Make STFT filters with no analysis and synthesis windows.
-    # kernel_size = fb_config['kernel_size']
-    enc, dec = make_enc_dec('stft', window=None, **fb_config)
-    # Input a boxcar function
-    inp = torch.ones(1, 1, 4096)
-    # Analysis-synthesis. Cut leading and trailing frames.
-    synth = dec(enc(inp))[:, :, kernel_size: -kernel_size]
-    # Assert that an boxcar input returns a boxcar output.
-    testing.assert_allclose(synth, inp[:, :, kernel_size: -kernel_size])
+@pytest.mark.parametrize("fb_config", fb_config_list())
+@pytest.mark.parametrize("analysis_window_name", [
+    'blackman', 'hamming', 'hann', 'bartlett', 'boxcar'
+])
+def test_perfect_resyn_window(fb_config, analysis_window_name):
+    """ Unit test perfect reconstruction """
+    kernel_size = fb_config['kernel_size']
+    window = get_window(analysis_window_name, kernel_size)
+
+    enc = Encoder(STFTFB(**fb_config, window=window))
+    # Compute window for perfect resynthesis
+    synthesis_window = perfect_synthesis_window(enc.filterbank.window,
+                                                enc.stride)
+    dec = Decoder(STFTFB(**fb_config, window=synthesis_window))
+    inp_wav = torch.ones(1, 1, 32000)
+    out_wav = dec(enc(inp_wav))[:, :, kernel_size: -kernel_size]
+    inp_test = inp_wav[:, :, kernel_size: -kernel_size]
+    testing.assert_allclose(inp_test,
+                            out_wav)
