284615b6769f04b555db5d305d7c78405f7846d6
vitrioil <opm249@gmail.com>
2020-03-09 20:29:48 +0530

    pysndfx, to add lowshelf postprocessing, renamed filter_audio to postprocess_audio

--- postprocess/postprocess_audio.py
+++ postprocess/postprocess_audio.py
@@ -4,18 +4,26 @@ import numpy as np
 import scipy.signal as sg
 import matplotlib.pyplot as plt
 
-import pydub
 import librosa
 import librosa.display
+from pysndfx import AudioEffectsChain
 
 from src.loader import convert_to_spectrogram
 
+afc = AudioEffectsChain()
 
 def filter_audio(y, sr=16_000, cutoff=15_000, low_cutoff=1, filter_order=5):
     sos = sg.butter(filter_order, [low_cutoff / sr / 2, cutoff / sr / 2], btype='band', analog=False, output='sos')
     filtered = sg.sosfilt(sos, y)
 
     return filtered
+
+def lowshelf(y, sr=16_000, gain=5, frequency=500, slope=0.5):
+    fx = afc.lowshelf(gain=gain, frequency=frequency, slope=slope)
+
+    y = fx(y, sample_in=sr, sample_out=sr)
+
+    return y
 
 def plot_wav(y, y_filt):
     y_spec = convert_to_spectrogram(y)
