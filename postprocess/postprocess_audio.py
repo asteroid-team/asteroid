709398a0010d7cd75cea8a879dd4b32ac4620d74
vitrioil <opm249@gmail.com>
2020-03-11 17:37:40 +0530

    high shelf with negative gain

--- postprocess/postprocess_audio.py
+++ postprocess/postprocess_audio.py
@@ -18,8 +18,8 @@ def filter_audio(y, sr=16_000, cutoff=15_000, low_cutoff=1, filter_order=5):
 
     return filtered
 
-def shelf(y, sr=16_000, gain=5, frequency=500, slope=0.5):
-    fx = afc.lowshelf(gain=gain, frequency=frequency, slope=slope)
+def shelf(y, sr=16_000, gain=5, frequency=500, slope=0.5, high_frequency=7_000):
+    fx = afc.lowshelf(gain=gain, frequency=frequency, slope=slope).highshelf(gain=-gain, frequency=high_frequency, slope=slope)
 
     y = fx(y, sample_in=sr, sample_out=sr)
 
