86c66aee808bcc844f452b039085659af3bfeda0
Efthymios Tzinis <etzinis2@illinois.edu>
2020-03-25 03:21:57 -0500

    Fixing paths for wham scripts and unziping command for noise

--- egs/wham/ConvTasNet/local/prepare_data.sh
+++ egs/wham/ConvTasNet/local/prepare_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wav_dir=tmp
-out_dir=tmp
+wav_dir=/mnt/data/wsj0-mix/wsj0
+out_dir=/mnt/data/wham
 python_path=python
 
 . utils/parse_options.sh
@@ -11,6 +11,7 @@ mkdir -p $out_dir
 echo "Download WHAM noises into $out_dir"
 # If downloading stalls for more than 20s, relaunch from previous state.
 wget -c --tries=0 --read-timeout=20 https://storage.googleapis.com/whisper-public/wham_noise.zip -P $out_dir
+unzip $out_dir/wham_noise.zip -d $out_dir
 
 echo "Download WHAM scripts into $out_dir"
 wget https://storage.googleapis.com/whisper-public/wham_scripts.tar.gz -P $out_dir
@@ -20,11 +21,9 @@ mv $out_dir/wham_scripts.tar.gz $out_dir/wham_scripts
 
 wait
 
-unzip $out_dir/wham_noise.zip $out_dir >> logs/unzip_wham.log
-
 echo "Run python scripts to create the WHAM mixtures"
 # Requires : Numpy, Scipy, Pandas, and Pysoundfile
-cd $out_dir/wham_scripts
+cd $out_dir/wham_scripts/wham_scripts
 $python_path create_wham_from_scratch.py \
 	--wsj0-root $wav_dir \
 	--wham-noise-root $out_dir/wham_noise\
