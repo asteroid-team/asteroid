c17247658b83e7d544ca6c2496741309535a6274
Efthymios Tzinis <etzinis2@illinois.edu>
2020-03-25 03:51:18 -0500

    Fixing paths for wham scripts and unziping command for noise (#66)

--- egs/wham/ConvTasNet/local/prepare_data.sh
+++ egs/wham/ConvTasNet/local/prepare_data.sh
@@ -11,16 +11,15 @@ mkdir -p $out_dir
 echo "Download WHAM noises into $out_dir"
 # If downloading stalls for more than 20s, relaunch from previous state.
 wget -c --tries=0 --read-timeout=20 https://storage.googleapis.com/whisper-public/wham_noise.zip -P $out_dir
+mkdir -p $out_dir/logs
+unzip $out_dir/wham_noise.zip -d $out_dir >> $out_dir/logs/unzip_wham.log
 
 echo "Download WHAM scripts into $out_dir"
 wget https://storage.googleapis.com/whisper-public/wham_scripts.tar.gz -P $out_dir
-mkdir -p $out_dir/wham_scripts
-tar -xzvf $out_dir/wham_scripts.tar.gz -C $out_dir/wham_scripts
+tar -xzvf $out_dir/wham_scripts.tar.gz -C $out_dir
 mv $out_dir/wham_scripts.tar.gz $out_dir/wham_scripts
 
 wait
-
-unzip $out_dir/wham_noise.zip $out_dir >> logs/unzip_wham.log
 
 echo "Run python scripts to create the WHAM mixtures"
 # Requires : Numpy, Scipy, Pandas, and Pysoundfile
