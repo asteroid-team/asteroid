00efb18d0673dc29fbdd23d0653f1f8b2d272313
Efthymios Tzinis <etzinis2@illinois.edu>
2020-04-08 20:26:12 -0500

    Removing the two step methods from the core system module

--- asteroid/engine/system.py
+++ asteroid/engine/system.py
@@ -20,14 +20,6 @@ class System(pl.LightningModule):
             of learning rate schedulers.
         config: Anything to be saved with the checkpoints during training.
             The config dictionary to re-instantiate the run for example.
-        two_step_approach (str):
-            None: then the two step approach is not used.
-            'separator': The two step is used for trainign the separation
-                         module only.
-            'filterbank': The two step approach is used for training only the
-                          adaptive encoder/decoder or in other words the
-                          filterbank.
-            For more info take a look at method common_step_two_step_separtion()
     .. note:: By default, `training_step` (used by `pytorch-lightning` in the
         training loop) and `validation_step` (used for the validation loop)
         share `common_step`. If you want different behavior for the training
@@ -35,8 +27,7 @@ class System(pl.LightningModule):
         `validation_step` instead.
     """
     def __init__(self, model, optimizer, loss_func, train_loader,
-                 val_loader=None, scheduler=None, config=None,
-                 two_step_approach=None):
+                 val_loader=None, scheduler=None, config=None):
         super().__init__()
         self.model = model
         self.optimizer = optimizer
@@ -44,12 +35,6 @@ class System(pl.LightningModule):
         self.train_loader = train_loader
         self.val_loader = val_loader
         self.scheduler = scheduler
-        if two_step_approach is not None:
-            assert (two_step_approach == 'filterbank' or
-                    two_step_approach == 'separator'), 'If the two-step ' \
-                   'approach is used then either filterbank or separator has ' \
-                   'to be used but got: {}'.format(two_step_approach)
-        self.two_step_approach = two_step_approach
         config = {} if config is None else config
         self.config = config
         # hparams will be logged to Tensorboard as text variables.
@@ -94,61 +79,6 @@ class System(pl.LightningModule):
         est_targets = self(inputs)
         loss = self.loss_func(est_targets, targets)
         return loss
-
-    def common_step_two_step_separtion(self, batch, mode='train'):
-        """ Common forward step between training and validation.
-
-        The function of this method is to unpack the data given by the loader,
-        forward the batch through the model and compute the loss for the
-        separation module and the filterbank when the optimization process
-        for source separation is breaken in two distinct processes as
-        proposed in [1].
-
-        Args:
-            batch: the object returned by the loader (a list of torch.Tensor
-                in most cases) but can be something else.
-            mode (str): 'train' or 'val'. In case of training or validation the
-                filterbank will return the estimated time signals. However,
-                in training mode the separator will be trained using the
-                ideal latent targets and it will estimate the corresponding
-                latent representations of the sources as proposed in [1].
-        Returns:
-            dict:
-
-            ``'loss'``: loss
-
-            ``'log'``: dict with tensorboard logs
-
-        References:
-            [1]: Tzinis, E., Venkataramani, S., Wang, Z., Subakan, Y. C., and
-                 Smaragdis, P., "Two-Step Sound Source Separation:
-                 Training on Learned Latent Targets." In Acoustics, Speech
-                 and Signal Processing (ICASSP), 2020 IEEE International
-                 Conference. https://arxiv.org/abs/1910.09804
-        """
-        mixture_time, true_sources_time = batch
-        if self.two_step_approach == 'filterbank':
-            est_sources_time, _ = self(mixture_time, true_sources_time)
-            return self.loss_func(est_sources_time, true_sources_time)
-
-        # Here we train or validate the separator. In training we need the
-        # latent targets to regress on. In validation we just provide the
-        # estimated time domain signals.
-        if mode == 'train':
-            latent_targets = self.model.get_ideal_latent_targets(
-                mixture_time, true_sources_time)
-            est_latents = self.model.estimate_latent_representations(
-                mixture_time)
-            batch_size, n_sources = est_latents.shape[0], est_latents.shape[1]
-            return self.loss_func(
-                est_latents.view(batch_size, n_sources, -1),
-                latent_targets.view(batch_size, n_sources, -1))
-        elif mode == 'val':
-            est_sources_time = self.model(mixture_time)
-            return self.loss_func(est_sources_time, true_sources_time)
-        else:
-            raise ValueError('The requested mode: {} is not available. '
-                             'Expected `train` or `val`.')
 
     def training_step(self, batch, batch_nb):
         """ Pass data through the model and compute the loss.
