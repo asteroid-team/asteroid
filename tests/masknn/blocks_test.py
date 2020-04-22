2e3378fe7e35295f8bf98f97247d324d6a4eeddf
mpariente <pariente.mnl@gmail.com>
2020-04-22 14:59:56 +0200

    [test] Extend test accordingly

--- tests/masknn/blocks_test.py
+++ tests/masknn/blocks_test.py
@@ -5,11 +5,12 @@ from asteroid.masknn import blocks
 
 @pytest.mark.parametrize("mask_act", ["relu", "softmax"])
 @pytest.mark.parametrize("out_chan", [None, 10])
-def test_tdconvnet(mask_act, out_chan):
+@pytest.mark.parametrize("skip_chan", [0, 12])
+def test_tdconvnet(mask_act, out_chan, skip_chan):
     in_chan, n_src = 20, 2
     model = blocks.TDConvNet(in_chan=in_chan, n_src=n_src, mask_act=mask_act,
                              n_blocks=2, n_repeats=2, bn_chan=10, hid_chan=11,
-                             skip_chan=12, out_chan=out_chan)
+                             skip_chan=skip_chan, out_chan=out_chan)
     batch, n_frames = 2, 24
     inp = torch.randn(batch, in_chan, n_frames)
     out = model(inp)
