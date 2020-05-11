8caaca03a8317bba7b65c321bb9a37aff501b17f
mpariente <pariente.mnl@gmail.com>
2020-05-11 22:00:13 +0200

    [egs] Remove file headers

--- egs/wham/DPRNN/local/preprocess_wham.py
+++ egs/wham/DPRNN/local/preprocess_wham.py
@@ -1,10 +1,3 @@
-"""
-WHAM directory preprocessing
-@author : Manuel Pariente, Inria-Nancy
-Modified and extended from github.com/kaituoxu/Conv-TasNet/
-MIT Copyright (c) 2018 Kaituo XU
-"""
-
 import argparse
 import json
 import os
