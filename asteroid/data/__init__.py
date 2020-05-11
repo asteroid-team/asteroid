3d3e68c01e03b8d7f9b6312ad102f1ae96acd09c
mpariente <pariente.mnl@gmail.com>
2020-05-11 21:11:21 +0200

    [src] Importable from asteroid/data

--- asteroid/data/__init__.py
+++ asteroid/data/__init__.py
@@ -2,3 +2,4 @@ from .wham_dataset import WhamDataset
 from .whamr_dataset import WhamRDataset
 from .dns_dataset import DNSDataset
 from .librimix_dataset import LibriMix
+from .wsj0_mix import Wsj0mixDataset
