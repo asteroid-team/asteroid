842589483249ec2ac1502067c3f4a585f9b7160f
Fabian-Robert Stöter <fabian-robert.stoter@inria.fr>
2020-05-22 19:07:31 +0200

    switch to google style docstrings

--- asteroid/data/musdb18_dataset.py
+++ asteroid/data/musdb18_dataset.py
@@ -7,90 +7,114 @@ import soundfile as sf
 
 
 class MUSDB18Dataset(torch.utils.data.Dataset):
-    """A dataset of that assumes audio sources to be stored
-    in track (subb)folder where each folder has a fixed number of sources.
-    For each track the users specifies a list of `source_files` and
-    a common `suffix`.
-    A linear mix is performed on the fly by summing the target and
-    the inferers up.
+    """MUSDB18 music separation dataset
+
+    The dataset consists of 150 full lengths music tracks (~10h duration) of
+    different genres along with their isolated stems:
+        `drums`, `bass`, `vocals` and `others`.
+
+    Out-of-the-box, asteroid does only support MUSDB18-HQ which comes as
+    uncompressed WAV files. To use the MUSDB18, please convert it to WAV first:
+
+    MUSDB18 HQ: https://zenodo.org/record/3338373   
+    MUSDB18     https://zenodo.org/record/1117372 
+
+    Note: The datasets are hosted on Zenodo and require that users
+          request access, since the tracks can only be used for
+          academic purposes. We manually check this requests.
+
+    This dataset asssumes music tracks in (sub)folders where each folder
+    has a fixed number of sources (defaults to 4). For each track, a list
+    of `sources` and a common `suffix` can be specified.
+    A linear mix is performed on the fly by summing up the sources
 
     Due to the fact that all tracks comprise the exact same set
-    of sources, random track mixing augmentation technique
-    can be used, where sources from different tracks are mixed
-    together.
+    of sources, random track mixing can be used can be used,
+    where sources from different tracks are mixed together.
 
-    Example
-    =======
-    train/1/vocals.wav ---------------\
-    train/1/drums.wav -----------------+--> input (mix), output[target]
-    train/1/bass.wav ------------------|
-    train/1/other.wav ----------------/
+    Folder Structure:
+        train/1/vocals.wav ---------------\
+        train/1/drums.wav -----------------+--> input (mix), output[target]
+        train/1/bass.wav ------------------|
+        train/1/other.wav ----------------/
+
+    Args:
+        root (str): Root path of dataset
+        sources (:obj:`list` of :obj:`str`, optional): List of source names.
+            Defaults to MUSDB18 4 stem scenario: `vocals`, `drums`, `bass`, `other`.
+        suffix (str, optional): Filename suffix, defaults to `.wav`.
+        split (str, optional): Dataset subfolder, defaults to `train`.
+        subset (:obj:`list` of :obj:`str`, optional): Selects a specific of
+            list of tracks to be loaded, defaults to `None` (loads all tracks).
+        seq_duration (float, optional): Duration of segments in seconds,
+            defaults to ``None`` which loads the full-length audio tracks.
+        samples_per_track (int, optional):
+            Number of samples yielded from each track, can be used to increase 
+            dataset size, defaults to `1`.
+        random_segments (boolean, optional): Enables random offset for track segments.
+        random_track_mix boolean: enables mixing of random sources from
+            different tracks to assemble mix.
+        source_augmentations (:obj:`list` of `obj`:`callable`):
+            list of augmentation function names,
+            defaults to no-op augmentations (input = output)
+        sample_rate (int, optional): Samplerate of files in dataset.
+
+    Attributes:
+        root (str): Root path of dataset
+        sources (:obj:`list` of :obj:`str`, optional): List of source names.
+            Defaults to MUSDB18 4 stem scenario: `vocals`, `drums`, `bass`, `other`.
+        suffix (str, optional): Filename suffix, defaults to `.wav`.
+        split (str, optional): Dataset subfolder, defaults to `train`.
+        subset (:obj:`list` of :obj:`str`, optional): Selects a specific of
+            list of tracks to be loaded, defaults to `None` (loads all tracks).
+        seq_duration (float, optional): Duration of segments in seconds,
+            defaults to ``None`` which loads the full-length audio tracks.
+        samples_per_track (int, optional):
+            Number of samples yielded from each track, can be used to increase 
+            dataset size, defaults to `1`.
+        random_segments (boolean, optional): Enables random offset for track segments.
+        random_track_mix boolean: enables mixing of random sources from
+            different tracks to assemble mix.
+        source_augmentations (:obj:`list` of `obj`:`callable`):
+            list of augmentation function names,
+            defaults to no-op augmentations (input = output)
+        sample_rate (int, optional): Samplerate of files in dataset.
+        tracks (:obj:`list` of :obj:`Dict`): List of track metadata
 
     """
     def __init__(self,
                  root,
+                 sources=['vocals', 'bass', 'drums', 'other'],
+                 suffix='.wav',
                  split='train',
                  subset=None,
-                 source_files=['vocals', 'bass', 'drums', 'other'],
-                 suffix='.wav',
                  seq_duration=None,
                  samples_per_track=1,
-                 random_chunks=False,
+                 random_segments=False,
                  random_track_mix=False,
                  source_augmentations=lambda audio: audio,
                  sample_rate=44100):
 
-        """MUSDB18 torch.data.Dataset that samples from the MUSDB18 tracks
-        using track and excerpts with replacement.
-
-        Parameters
-        ----------
-        root : str
-            root path of MUSDB18HQ, please download from zenodo
-        suffix : str
-            specify the filename suffice, defaults to `.wav`
-        split : str
-            select dataset subfolder, defaults to ``train``.
-        subsets : list(str)
-            selects a list of track folders, defaults to `None` 
-            which loads all tracks
-        seq_duration : float
-            training is performed in chunks of ``seq_duration`` (in seconds,
-            defaults to ``None`` which loads the full audio track
-        samples_per_track : int
-            sets the number of samples, yielded from each track
-            Defaults to 1
-        source_augmentations : list[callables]
-            provide list of augmentation function that take a multi-channel
-            audio file of shape (src, samples) as input and output. Defaults to
-            no-augmentations (input = output)
-        random_track_mix : boolean
-            randomly mixes sources from different tracks to assemble a
-            custom mix. This augmenation is only applied for the train subset.
-        args, kwargs : additional keyword arguments
-            used to add further control for the musdb dataset
-            initialization function.
-        """
         self.root = Path(root).expanduser()
         self.split = split
         self.sample_rate = sample_rate
         self.seq_duration = seq_duration
         self.random_track_mix = random_track_mix
-        self.random_chunks = random_chunks
+        self.random_segments = random_segments
         self.source_augmentations = source_augmentations
-        self.source_files = source_files
+        self.sources = sources
         self.suffix = suffix
         self.subset = subset
         self.samples_per_track = samples_per_track
         self.tracks = list(self.get_tracks())
         if not self.tracks:
-            raise RuntimeError("No tracks found")
+            raise RuntimeError("No tracks found.")
 
     def __getitem__(self, index):
         # assemble the mixture of target and interferers
         audio_sources = {}
         # load interferers
-        for source in self.source_files:
+        for source in self.sources:
             # optionally select a random track for each source
             if self.random_track_mix:
                 track_id = random.choice(range(len(self.tracks)))
@@ -98,7 +122,7 @@ class MUSDB18Dataset(torch.utils.data.Dataset):
                 track_id = index // self.samples_per_track
 
             track_path = self.tracks[track_id]['path']
-            if self.random_chunks:
+            if self.random_segments:
                 min_duration = self.tracks[track_id]['min_duration']
                 start = random.uniform(0, min_duration - self.seq_duration)
 
@@ -139,7 +163,7 @@ class MUSDB18Dataset(torch.utils.data.Dataset):
                     continue
 
                 source_paths = [
-                    track_path / (s + self.suffix) for s in self.source_files
+                    track_path / (s + self.suffix) for s in self.sources
                 ]
                 if not all(sp.exists() for sp in source_paths):
                     print(
