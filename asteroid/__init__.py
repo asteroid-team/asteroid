17310b9a17d4cb06eb71c61f9356870217dc677b
mpariente <pariente.mnl@gmail.com>
2020-05-29 22:33:26 +0200

    [src] Globally import models

--- asteroid/__init__.py
+++ asteroid/__init__.py
@@ -1,5 +1,7 @@
 import pathlib
 from .utils import deprecation_utils, torch_utils
 
+from .models import ConvTasNet, DPRNNTasNet
+
 project_root = str(pathlib.Path(__file__).expanduser().absolute().parent.parent)
 __version__ = '0.2.1'
