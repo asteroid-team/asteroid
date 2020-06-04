7154df373433e7b5c3c0f9dae1a94d13c7433502
Mathieu Hu <mathieu.hu@inria.fr>
2020-06-04 17:58:24 +0200

    fix current_dir creation. Added deprecation warning for useless variable n_src

--- egs/librimix/ConvTasNet/local/prepare_data.sh
+++ egs/librimix/ConvTasNet/local/prepare_data.sh
@@ -6,11 +6,16 @@ python_path=python
 
 . ./utils/parse_options.sh
 
-current_dir= pwd
+current_dir=$(pwd)
 # Clone LibriMix repo
 git clone https://github.com/JorisCos/LibriMix
 
 # Run generation script
+if [ -n $n_src ]; then
+  echo "DEPRECATION WARNING: n_src has no effect on the generated data and"
+  echo "will be removed from future release"
+fi
+
 cd LibriMix
 . generate_librimix.sh $storage_dir $n_src
 
