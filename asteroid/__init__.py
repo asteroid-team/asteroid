c2154229bdf5e663b96af821436972d0fcaeea3a
mpariente <pariente.mnl@gmail.com>
2020-06-16 21:43:44 +0200

    Release v0.3.0

--- asteroid/__init__.py
+++ asteroid/__init__.py
@@ -3,7 +3,7 @@ from .utils import deprecation_utils, torch_utils
 from .models import ConvTasNet, DPRNNTasNet
 
 project_root = str(pathlib.Path(__file__).expanduser().absolute().parent.parent)
-__version__ = '0.3.0rc1'
+__version__ = '0.3.0'
 
 
 def show_available_models():
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-asteroid_version = "0.3.0rc1"
+asteroid_version = "0.3.0"
 
 with open("README.md", encoding='utf-8') as fh:
     long_description = fh.read()
