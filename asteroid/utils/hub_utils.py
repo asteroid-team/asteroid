6fafc2ac3e14c3ca5ac88ddb0aa17a9d8842d6ee
mpariente <pariente.mnl@gmail.com>
2020-06-22 22:44:35 +0200

    [models] Update available models

--- asteroid/utils/hub_utils.py
+++ asteroid/utils/hub_utils.py
@@ -10,6 +10,7 @@ CACHE_DIR = os.getenv(
 MODELS_URLS_HASHTABLE = {
     'mpariente/ConvTasNet_WHAM!_sepclean': 'https://zenodo.org/record/3862942/files/model.pth?download=1',
     'mpariente/DPRNNTasNet_WHAM!_sepclean': 'https://zenodo.org/record/3873670/files/model.pth?download=1',
+    'mpariente/DPRNNTasNet(ks=16)_WHAM!_sepclean': 'https://zenodo.org/record/3903795/files/model.pth?download=1',
     'Cosentino/ConvTasNet_LibriMix_sep_clean': 'https://zenodo.org/record/3873572/files/model.pth?download=1',
     'Cosentino/ConvTasNet_LibriMix_sep_noisy': 'https://zenodo.org/record/3874420/files/model.pth?download=1',
 }
