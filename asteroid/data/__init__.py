a4080185ecf65d6fe19cabf2af009d3f390146f1
jensheit <heitkaemper@nt.upb.de>
2020-07-23 14:37:26 +0200

    adds Dataset for SmsWsj

--- asteroid/data/__init__.py
+++ asteroid/data/__init__.py
@@ -4,5 +4,6 @@ from .dns_dataset import DNSDataset
 from .librimix_dataset import LibriMix
 from .wsj0_mix import Wsj0mixDataset
 from .musdb18_dataset import MUSDB18Dataset
+from .sms_wsj_dataset import SmsWsjDataset
 from .kinect_wsj import KinectWsjMixDataset
 
