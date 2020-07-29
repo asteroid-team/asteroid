e5c7153206c14cb0be6ed264579e3e922c8de25a
Joris Cosentino <joris.cosentino.pro@gmail.com>
2020-07-29 10:25:11 +0200

    Update test

--- tests/engine/gan_system_test.py
+++ tests/engine/gan_system_test.py
@@ -21,13 +21,13 @@ class DummyDataset(data.Dataset):
 
 
 class GeneratorLoss(_Loss):
-    def forward(self, estimates, targets, est_labels):
+    def forward(self, est_labels):
         loss = torch.mean((est_labels - torch.ones_like(est_labels)) ** 2)
         return loss
 
 
 class DiscriminatorLoss(_Loss):
-    def forward(self, inputs, targets, estimates, est_labels, labels):
+    def forward(self, est_labels, labels):
         # Behaves differently if estimates come from  the generated data or not
         if labels:
             loss = torch.mean((est_labels - torch.ones_like(est_labels)) ** 2)
@@ -42,15 +42,83 @@ class Discriminator(nn.Module):
         super().__init__()
         self.model = nn.Sequential(nn.Linear(10, 1), nn.Sigmoid())
 
-    def forward(self, x, y, z):
+    def forward(self, x):
         """
         Forward pass of discriminator.
         Args:
             x: batch of estimates
-            y: batch of targets
-            z: batch of inputs
         """
         return self.model(x)
+
+
+class TrainGAN (GanSystem):
+
+    def training_step(self, batch, batch_nb, optimizer_idx):
+        # Get data from data_loader
+        inputs, targets = batch
+        # Forward inputs
+        estimates = self(inputs)
+        # Train discriminator
+        if optimizer_idx == 0:
+            # Compute D loss for targets
+            est_true_labels = self.discriminator(targets)
+            true_loss = self.d_loss(est_true_labels, True)
+            # Compute D loss for self.estimates
+            est_false_labels = self.discriminator(estimates.detach())
+            fake_loss = self.d_loss(est_false_labels, False)
+            # Overall, the loss is the mean of these
+            d_loss = (true_loss + fake_loss) * 0.5
+            tqdm_dict = {'d_loss': d_loss}
+            output = {
+                'loss': d_loss,
+                'progress_bar': tqdm_dict,
+                'log': tqdm_dict
+            }
+            return output
+
+        # Train generator
+        if optimizer_idx == 1:
+            # The generator is supposed to fool the discriminator.
+            est_labels = self.discriminator(estimates)
+            adversarial_loss = self.g_loss(est_labels)
+            tqdm_dict = {'g_loss': adversarial_loss}
+            output = {
+                'loss': adversarial_loss,
+                'progress_bar': tqdm_dict,
+                'log': tqdm_dict
+            }
+            return output
+
+    def validation_step(self, batch, batch_nb):
+        """ Need to overwrite PL validation_step to do validation.
+        Args:
+            batch: the object returned by the loader (a list of torch.Tensor
+                in most cases) but can be something else.
+            batch_nb (int): The number of the batch in the epoch.
+        Returns:
+            dict:
+            ``'val_loss'``: loss
+        """
+        inputs, targets = batch
+        est_targets = self(inputs)
+        val_loss = self.validation_loss(est_targets, targets)
+        return {'val_loss': val_loss}
+
+    def validation_epoch_end(self, outputs):
+        """ How to aggregate outputs of `validation_step` for logging.
+        Args:
+           outputs (list[dict]): List of validation losses, each with a
+           ``'val_loss'`` key
+        Returns:
+            dict: Average loss
+            ``'val_loss'``: Average loss on `outputs`
+            ``'log'``: Tensorboard logs
+            ``'progress_bar'``: Tensorboard logs
+        """
+        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+        tensorboard_logs = {'val_loss': avg_loss}
+        return {'val_loss': avg_loss, 'log': tensorboard_logs,
+                'progress_bar': tensorboard_logs}
 
 
 def test_system():
@@ -65,10 +133,10 @@ def test_system():
     validation_loss = PITLossWrapper(pairwise_neg_sisdr, pit_from='pw_mtx')
     dataset = DummyDataset()
     loader = data.DataLoader(dataset, batch_size=4, num_workers=4)
-    gan = GanSystem(discriminator=discriminator, generator=generator,
-                    opt_d=opt_d, opt_g=opt_g, scheduler_d=scheduler_d,
-                    scheduler_g=scheduler_g, discriminator_loss=d_loss,
+    gan = TrainGAN(discriminator=discriminator, generator=generator,
+                    opt_d=opt_d, opt_g=opt_g,  discriminator_loss=d_loss,
                     generator_loss=g_loss, validation_loss=validation_loss,
-                    train_loader=loader, val_loader=loader)
+                    train_loader=loader, val_loader=loader,
+                    scheduler_d=scheduler_d, scheduler_g=scheduler_g)
     trainer = Trainer(max_epochs=1, fast_dev_run=True)
     trainer.fit(gan)
