3b935d178baf69d857104ca81d782b94df64c342
Pariente Manuel <pariente.mnl@gmail.com>
2020-08-11 20:35:16 +0200

    Apply suggestions from code review

--- tests/dsp/overlap_add_test.py
+++ tests/dsp/overlap_add_test.py
@@ -11,9 +11,10 @@ from asteroid.dsp.overlap_add import OverlapAddWrapper
 @pytest.mark.parametrize("window", ["hanning"])
 @pytest.mark.parametrize("window_size", [128])
 @pytest.mark.parametrize("hop_size", [64])
-def test_consistency_noweight(length, batch_size, n_src, window, window_size, hop_size):
+def test_overlap_add(length, batch_size, n_src, window, window_size, hop_size):
     mix = torch.randn((batch_size, length)).reshape(batch_size, 1, -1)
     nnet = lambda x: x.unsqueeze(1).repeat(1, n_src, 1)
     oladd = OverlapAddWrapper(nnet, n_src, window_size, hop_size, window)
     oladded = oladd(mix)
     assert_allclose(mix.repeat(1, n_src, 1), oladded)
+    
