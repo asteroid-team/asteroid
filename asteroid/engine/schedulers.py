624a0ce7516c7a9b651f250db20dcd40f57fd098
popcornell <cornellsamuele@gmail.com>
2020-08-13 15:07:26 +0200

    added some schedulers

--- asteroid/engine/schedulers.py
+++ asteroid/engine/schedulers.py
@@ -1,5 +1,5 @@
 
-class BaseScheduler(object):
+class _BaseScheduler(object):
     def __init__(self, optimizer):
         self.optimizer = optimizer
         self.step_num = 0
@@ -7,14 +7,17 @@ class BaseScheduler(object):
     def zero_grad(self):
         self.optimizer.zero_grad()
 
-    def _update_lr(self):
-        self.step_num += 1
-        for param_group in self.optimizer.param_groups:
-            param_group['lr'] = param_group['lr'] # change here your lr
+    def _get_lr(self):
         raise NotImplementedError
 
+    def _set_lr(self, lr):
+        for param_group in self.optimizer.param_groups:
+            param_group['lr'] = lr
+
     def step(self):
-        self._update_lr()
+        self.step_num += 1
+        lr = self._get_lr()
+        self._set_lr(lr)
         self.optimizer.step()
 
     def load_state_dict(self, state_dict):
@@ -22,3 +25,41 @@ class BaseScheduler(object):
 
     def state_dict(self):
         return {key: value for key, value in self.__dict__.items()}
+
+
+
+class NoamScheduler(_BaseScheduler):
+    def __init__(self, optimizer, d_model, warmup_steps, scale=1.0):
+        super(NoamScheduler, self).__init__(optimizer)
+        self.d_model = d_model
+        self.scale = scale
+        self.warmup_steps = warmup_steps
+
+    def _get_lr(self):
+        lr = self.scale * self.d_model ** (-0.5) * \
+                min(self.step_num ** (-0.5), self.step_num * self.warmup_steps ** (-1.5))
+        return lr
+
+
+class DPTNetScheduler(_BaseScheduler):
+    def __init__(self, optimizer, steps_per_epoch, d_model, warmup_steps, scale=1.0):
+        super(DPTNetScheduler, self).__init__(optimizer)
+        self.scale = scale
+        self.d_model = d_model
+        self.warmup_steps = warmup_steps
+        self.steps_per_epoch = steps_per_epoch
+        self.epoch = None
+
+    def _get_lr(self):
+        if self.step_num % self.steps_per_epoch == 0:
+            self.epoch += 1
+
+        if self.step_num > self.warmup_steps:
+            # exp decaying
+            lr = 0.0004 * (0.98 ** ((self.epoch - 1) // 2))
+        else:
+            # noam
+            lr = self.scale * self.d_model ** (-0.5) * \
+                min(self.step_num ** (-0.5), self.step_num * self.warmup_steps ** (-1.5))
+        return lr
+
