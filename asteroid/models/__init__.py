d8d1a33c373ec5b77baceeac1444f1b050a04cfb
mpariente <pariente.mnl@gmail.com>
2020-08-17 19:25:21 +0200

    [src] Globally import SudoRMRF models

--- asteroid/models/__init__.py
+++ asteroid/models/__init__.py
@@ -1,6 +1,7 @@
 # Models
 from .conv_tasnet import ConvTasNet
 from .dprnn_tasnet import DPRNNTasNet
+from .sudormrf import SuDORMRFImproved, SuDORMRF
 
 # Sharing-related
 from .publisher import save_publishable, upload_publishable
@@ -8,6 +9,8 @@ from .publisher import save_publishable, upload_publishable
 __all__ = [
     "ConvTasNet",
     "DPRNNTasNet",
+    "SuDORMRFImproved",
+    "SuDORMRF",
     "save_publishable",
     "upload_publishable",
 ]
