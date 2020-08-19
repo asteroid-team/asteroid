65831df1db3902613b214bff378fbac4f519836a
Pariente Manuel <pariente.mnl@gmail.com>
2020-08-19 09:38:28 +0200

    [src] Add FUSS dataset from FUSS PR (#215)

--- asteroid/data/__init__.py
+++ asteroid/data/__init__.py
@@ -6,6 +6,7 @@ from .wsj0_mix import Wsj0mixDataset
 from .musdb18_dataset import MUSDB18Dataset
 from .sms_wsj_dataset import SmsWsjDataset
 from .kinect_wsj import KinectWsjMixDataset
+from .fuss_dataset import FUSSDataset
 
 __all__ = [
     "WhamDataset",
@@ -16,4 +17,5 @@ __all__ = [
     "MUSDB18Dataset",
     "SmsWsjDataset",
     "KinectWsjMixDataset",
+    "FUSSDataset",
 ]
