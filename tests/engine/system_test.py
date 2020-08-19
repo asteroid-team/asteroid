0247b1f04a0ebff2420673a76f569632689944c5
mpariente <pariente.mnl@gmail.com>
2020-08-19 23:58:19 +0200

    Update system_test.py

--- tests/engine/system_test.py
+++ tests/engine/system_test.py
@@ -1,21 +1,9 @@
-import torch
 from torch import nn, optim
 from torch.utils import data
 from pytorch_lightning import Trainer
 
 from asteroid.engine.system import System
-
-
-class DummyDataset(data.Dataset):
-    def __init__(self):
-        self.inp_dim = 10
-        self.out_dim = 10
-
-    def __len__(self):
-        return 20
-
-    def __getitem__(self, idx):
-        return torch.randn(1, self.inp_dim), torch.randn(1, self.out_dim)
+from asteroid.utils.test_utils import DummyDataset
 
 
 def test_system():
