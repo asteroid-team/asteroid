64c65702752ae47167acfb1bb42b7f0884ef324d
mpariente <pariente.mnl@gmail.com>
2020-08-22 21:25:28 +0200

    Add asteroid.models.get

--- asteroid/models/__init__.py
+++ asteroid/models/__init__.py
@@ -18,3 +18,21 @@ __all__ = [
     "save_publishable",
     "upload_publishable",
 ]
+
+
+def get(identifier):
+    """ Returns an model class from a string (case-insensitive).
+
+    Args:
+        identifier (str): the model name.
+
+    Returns:
+        :class:`torch.nn.Module`
+    """
+    if isinstance(identifier, str):
+        to_get = {k.lower(): v for k, v in globals().items()}
+        cls = to_get.get(identifier.lower())
+        if cls is None:
+            raise ValueError(f"Could not interpret model name : {str(identifier)}")
+        return cls
+    raise ValueError(f"Could not interpret model name : {str(identifier)}")
