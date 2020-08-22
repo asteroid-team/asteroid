3f508864c55f4805caee7399f8fcadc8ac718caa
mpariente <pariente.mnl@gmail.com>
2020-08-22 21:57:40 +0200

    Add register_model method

--- asteroid/models/__init__.py
+++ asteroid/models/__init__.py
@@ -20,6 +20,21 @@ __all__ = [
 ]
 
 
+def register_model(custom_model):
+    """ Register a custom model, gettable with `models.get`.
+
+    Args:
+        custom_model: Custom model to register.
+
+    """
+    if (
+        custom_model.__name__ in globals().keys()
+        or custom_model.__name__.lower() in globals().keys()
+    ):
+        raise ValueError(f"Model {custom_model.__name__} already exists. Choose another name.")
+    globals().update({custom_model.__name__: custom_model})
+
+
 def get(identifier):
     """ Returns an model class from a string (case-insensitive).
 
