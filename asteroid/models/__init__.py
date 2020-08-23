899b945781164a4a199ee139f53ce375011afffd
Pariente Manuel <pariente.mnl@gmail.com>
2020-08-23 09:28:27 +0200

    [src] Refactor BaseTasnNet in two: better loadable/serializable models  (#232)

--- asteroid/models/__init__.py
+++ asteroid/models/__init__.py
@@ -18,3 +18,36 @@ __all__ = [
     "save_publishable",
     "upload_publishable",
 ]
+
+
+def register_model(custom_model):
+    """ Register a custom model, gettable with `models.get`.
+
+    Args:
+        custom_model: Custom model to register.
+
+    """
+    if (
+        custom_model.__name__ in globals().keys()
+        or custom_model.__name__.lower() in globals().keys()
+    ):
+        raise ValueError(f"Model {custom_model.__name__} already exists. Choose another name.")
+    globals().update({custom_model.__name__: custom_model})
+
+
+def get(identifier):
+    """ Returns an model class from a string (case-insensitive).
+
+    Args:
+        identifier (str): the model name.
+
+    Returns:
+        :class:`torch.nn.Module`
+    """
+    if isinstance(identifier, str):
+        to_get = {k.lower(): v for k, v in globals().items()}
+        cls = to_get.get(identifier.lower())
+        if cls is None:
+            raise ValueError(f"Could not interpret model name : {str(identifier)}")
+        return cls
+    raise ValueError(f"Could not interpret model name : {str(identifier)}")
--- asteroid/models/base_models.py
+++ asteroid/models/base_models.py
@@ -7,7 +7,162 @@ from ..utils.hub_utils import cached_download
 from ..masknn import activations
 
 
-class BaseTasNet(nn.Module):
+class BaseModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, *args, **kwargs):
+        raise NotImplementedError
+
+    @torch.no_grad()
+    def separate(self, wav, *args, **kwargs):
+        """ Infer separated sources from input waveforms.
+        Also supports filenames.
+
+        Args:
+            wav (Union[torch.Tensor, numpy.ndarray, str]): waveform array/tensor.
+                Shape: 1D, 2D or 3D tensor, time last.
+
+        Returns:
+            Union[torch.Tensor, numpy.ndarray, None], the estimated sources.
+                (batch, n_src, time) or (n_src, time) w/o batch dim.
+
+        .. note::
+            By default, `separate` calls `_separate` which calls `forward`.
+            For models whose `forward` doesn't return waveform tensors,
+            overwrite `_separate` to return waveform tensors.
+        """
+        # Handle filename inputs
+        was_file = False
+        if isinstance(wav, str):
+            import soundfile as sf
+
+            was_file = True
+            filename = wav
+            wav, fs = sf.read(wav, dtype="float32")
+            wav = torch.from_numpy(wav)
+        # Handle numpy inputs
+        was_numpy = False
+        if isinstance(wav, np.ndarray):
+            was_numpy = True
+            wav = torch.from_numpy(wav)
+        # Handle device placement
+        input_device = wav.device
+        model_device = next(self.parameters()).device
+        wav = wav.to(model_device)
+        # Forward
+        out_wavs = self._separate(wav, *args, **kwargs)
+        # Back to input device (and numpy if necessary)
+        out_wavs = out_wavs.to(input_device)
+        if was_numpy:
+            return out_wavs.cpu().data.numpy()
+        if was_file:
+            # Save wav files to filename_est1.wav etc...
+            to_save = out_wavs.cpu().data.numpy()
+            for src_idx, est_src in enumerate(to_save):
+                base = ".".join(filename.split(".")[:-1])
+                save_name = base + "_est{}.".format(src_idx + 1) + filename.split(".")[-1]
+                sf.write(save_name, est_src, fs)
+            return
+        return out_wavs
+
+    def _separate(self, wav, *args, **kwargs):
+        """ Hidden separation method
+
+        Args:
+            wav (Union[torch.Tensor, numpy.ndarray, str]): waveform array/tensor.
+                Shape: 1D, 2D or 3D tensor, time last.
+
+        Returns:
+            The output of self(wav, *args, **kwargs).
+        """
+        return self(wav, *args, **kwargs)
+
+    @classmethod
+    def from_pretrained(cls, pretrained_model_conf_or_path, *args, **kwargs):
+        """ Instantiate separation model from a model config (file or dict).
+
+        Args:
+            pretrained_model_conf_or_path (Union[dict, str]): model conf as
+                returned by `serialize`, or path to it. Need to contain
+                `model_args` and `state_dict` keys.
+            *args: Positional arguments to be passed to the model.
+            **kwargs: Keyword arguments to be passed to the model.
+                They overwrite the ones in the model package.
+
+        Returns:
+            nn.Module corresponding to the pretrained model conf/URL.
+
+        Raises:
+            ValueError if the input config file doesn't contain the keys
+                `model_name`, `model_args` or `state_dict`.
+        """
+        from . import get  # Avoid circular imports
+
+        if isinstance(pretrained_model_conf_or_path, str):
+            cached_model = cached_download(pretrained_model_conf_or_path)
+            conf = torch.load(cached_model, map_location="cpu")
+        else:
+            conf = pretrained_model_conf_or_path
+
+        if "model_name" not in conf.keys():
+            raise ValueError(
+                "Expected config dictionary to have field "
+                "model_name`. Found only: {}".format(conf.keys())
+            )
+        if "state_dict" not in conf.keys():
+            raise ValueError(
+                "Expected config dictionary to have field "
+                "state_dict`. Found only: {}".format(conf.keys())
+            )
+        if "model_args" not in conf.keys():
+            raise ValueError(
+                "Expected config dictionary to have field "
+                "model_args`. Found only: {}".format(conf.keys())
+            )
+        conf["model_args"].update(kwargs)  # kwargs overwrite config.
+        # Attempt to find the model and instantiate it.
+        try:
+            model_class = get(conf["model_name"])
+            model = model_class(*args, **conf["model_args"])
+        except ValueError:  # Couldn't get the model, maybe custom.
+            model = cls(*args, **conf["model_args"])  # Child class.
+        model.load_state_dict(conf["state_dict"])
+        return model
+
+    def serialize(self):
+        """ Serialize model and output dictionary.
+
+        Returns:
+            dict, serialized model with keys `model_args` and `state_dict`.
+        """
+        from .. import __version__ as asteroid_version  # Avoid circular imports
+        import pytorch_lightning as pl  # Not used in torch.hub
+
+        model_conf = dict(
+            model_name=self.__class__.__name__,
+            state_dict=self.get_state_dict(),
+            model_args=self.get_model_args(),
+        )
+        # Additional infos
+        infos = dict()
+        infos["software_versions"] = dict(
+            torch_version=torch.__version__,
+            pytorch_lightning_version=pl.__version__,
+            asteroid_version=asteroid_version,
+        )
+        model_conf["infos"] = infos
+        return model_conf
+
+    def get_state_dict(self):
+        """ In case the state dict needs to be modified before sharing the model."""
+        return self.state_dict()
+
+    def get_model_args(self):
+        raise NotImplementedError
+
+
+class BaseTasNet(BaseModel):
     """ Base class for encoder-masker-decoder separation models.
 
     Args:
@@ -53,111 +208,8 @@ class BaseTasNet(nn.Module):
             return out_wavs.squeeze(0)
         return out_wavs
 
-    def separate(self, wav):
-        """ Infer separated sources from input waveforms.
-        Also supports filenames.
-
-        Args:
-            wav (Union[torch.Tensor, numpy.ndarray, str]): waveform array/tensor.
-                Shape: 1D, 2D or 3D tensor, time last.
-
-        Returns:
-            Union[torch.Tensor, numpy.ndarray, None], the estimated sources.
-                (batch, n_src, time) or (n_src, time) w/o batch dim.
-        """
-        with torch.no_grad():
-            return self._separate(wav)
-
-    def _separate(self, wav):
-        """ Hidden separation method
-
-        Args:
-            wav (Union[torch.Tensor, numpy.ndarray, str]): waveform array/tensor.
-                Shape: 1D, 2D or 3D tensor, time last.
-
-        Returns:
-            Union[torch.Tensor, numpy.ndarray, None], the estimated sources.
-                (batch, n_src, time) or (n_src, time) w/o batch dim.
-        """
-        # Handle filename inputs
-        was_file = False
-        if isinstance(wav, str):
-            import soundfile as sf
-
-            was_file = True
-            filename = wav
-            wav, fs = sf.read(wav, dtype="float32")
-            wav = torch.from_numpy(wav)
-        # Handle numpy inputs
-        was_numpy = False
-        if isinstance(wav, np.ndarray):
-            was_numpy = True
-            wav = torch.from_numpy(wav)
-        # Handle device placement
-        input_device = wav.device
-        model_device = next(self.parameters()).device
-        wav = wav.to(model_device)
-        # Forward
-        out_wavs = self.forward(wav)
-        # Back to input device (and numpy if necessary)
-        out_wavs = out_wavs.to(input_device)
-        if was_numpy:
-            return out_wavs.cpu().data.numpy()
-        if was_file:
-            # Save wav files to filename_est1.wav etc...
-            to_save = out_wavs.cpu().data.numpy()
-            for src_idx, est_src in enumerate(to_save):
-                base = ".".join(filename.split(".")[:-1])
-                save_name = base + "_est{}.".format(src_idx + 1) + filename.split(".")[-1]
-                sf.write(save_name, est_src, fs)
-            return
-        return out_wavs
-
-    @classmethod
-    def from_pretrained(cls, pretrained_model_conf_or_path, *args, **kwargs):
-        """ Instantiate separation model from a model config (file or dict).
-
-        Args:
-            pretrained_model_conf_or_path (Union[dict, str]): model conf as
-                returned by `serialize`, or path to it. Need to contain
-                `model_args` and `state_dict` keys.
-
-        Returns:
-            Instance of BaseTasNet
-
-        Raises:
-            ValueError if the input config file doesn't contain the keys
-                `model_args` and `state_dict`.
-        """
-        if isinstance(pretrained_model_conf_or_path, str):
-            cached_model = cached_download(pretrained_model_conf_or_path)
-            conf = torch.load(cached_model, map_location="cpu")
-        else:
-            conf = pretrained_model_conf_or_path
-        if "model_args" not in conf.keys():
-            raise ValueError(
-                "Expected config dictionary to have field "
-                "model_args`. Found only: {}".format(conf.keys())
-            )
-        if "state_dict" not in conf.keys():
-            raise ValueError(
-                "Expected config dictionary to have field "
-                "state_dict`. Found only: {}".format(conf.keys())
-            )
-        model = cls(*args, **conf["model_args"], **kwargs)
-        model.load_state_dict(conf["state_dict"])
-        return model
-
-    def serialize(self):
-        """ Serialize model and output dictionary.
-
-        Returns:
-            dict, serialized model with keys `model_args` and `state_dict`.
-        """
-        from .. import __version__ as asteroid_version  # Avoid circular imports
-        import pytorch_lightning as pl  # Not used in torch.hub
-
-        model_conf = dict()
+    def get_model_args(self):
+        """ Arguments needed to re-instantiate the model. """
         fb_config = self.encoder.filterbank.get_config()
         masknet_config = self.masker.get_config()
         # Assert both dict are disjoint
@@ -166,19 +218,9 @@ class BaseTasNet(nn.Module):
                 "Filterbank and Mask network config share" "common keys. Merging them is not safe."
             )
         # Merge all args under model_args.
-        model_conf["model_name"] = self.__class__.__name__
-        model_conf["model_args"] = {
+        model_args = {
             **fb_config,
             **masknet_config,
             "encoder_activation": self.encoder_activation,
         }
-        model_conf["state_dict"] = self.state_dict()
-        # Additional infos
-        infos = dict()
-        infos["software_versions"] = dict(
-            torch_version=torch.__version__,
-            pytorch_lightning_version=pl.__version__,
-            asteroid_version=asteroid_version,
-        )
-        model_conf["infos"] = infos
-        return model_conf
+        return model_args
