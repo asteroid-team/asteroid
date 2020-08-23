7e42a01c25718767a8a1594182aa599bbc50bcec
Fabian-Robert Stöter <fabian-robert.stoter@inria.fr>
2020-08-23 22:46:03 +0200

    cleanup

--- tests/models/jit_test.py
+++ tests/models/jit_test.py
@@ -8,9 +8,6 @@ from torch.testing._internal.jit_utils import JitTestCase
 class TestModels(JitTestCase):
     @staticmethod
     def _test_demask(self, device, check_export_import=True):
-
-        example = torch.rand((1, 800), device=device)
-
         # set model to eval due to non-deterministic behaviour of dropout
         model = DeMask().eval().to(device)
 
@@ -18,4 +15,4 @@ class TestModels(JitTestCase):
         torch.jit.script(model)
 
     def test_demask(self):
-        self._test_demask(self, device='cpu')
+        self._test_demask(self, device="cpu")
