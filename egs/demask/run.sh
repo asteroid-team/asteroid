fdd0e48bb69673f367c3802b07c5d14a9e51436d
Pariente Manuel <pariente.mnl@gmail.com>
2020-08-25 21:08:16 +0200

    Update egs/demask/run.sh

--- egs/demask/run.sh
+++ egs/demask/run.sh
@@ -20,7 +20,7 @@ python_path=python
 # ./run.sh --stage 3 --tag my_tag --task sep_noisy --id 0,1
 
 # General
-stage=3  # Controls from which stage to start
+stage=0  # Controls from which stage to start
 tag=""  # Controls the directory name associated to the experiment
 # You can ask for several GPUs using id (passed to CUDA_VISIBLE_DEVICES)
 id=$CUDA_VISIBLE_DEVICES
