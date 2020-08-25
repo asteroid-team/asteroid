b12e62480467d955a69972b2f275eebaddeec754
mpariente <pariente.mnl@gmail.com>
2020-08-25 13:56:17 +0200

    [hub] DeMask importable from TorchHub

--- asteroid/filterbanks/transforms.py
+++ asteroid/filterbanks/transforms.py
@@ -1,5 +1,4 @@
 import torch
-import numpy as np
 
 EPS = 1e-8
 
@@ -226,6 +225,8 @@ def from_numpy(array, dim=-2):
         :class:`torch.Tensor`:
             Corresponding torch.Tensor (complex axis in dim `dim`=
     """
+    import numpy as np  # Hub-importable
+
     return torch.cat([torch.from_numpy(np.real(array)), torch.from_numpy(np.imag(array))], dim=dim)
 
 
