78799064cbb3c1760dc8bc147b75f2f867b15e56
HU Mathieu <mathieu.hu@inria.fr>
2020-09-04 16:48:05 +0200

    Format fix with black

--- tests/models/jit_test.py
+++ tests/models/jit_test.py
@@ -5,59 +5,54 @@ from asteroid.models import DeMask, ConvTasNet, DPRNNTasNet, DPTNet, LSTMTasNet
 from torch.testing._internal.jit_utils import JitTestCase
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def small_model_params():
     params = {
         ConvTasNet.__name__: {
-            'n_src': 2,
-            'n_repeats': 2,
-            'n_blocks': 3,
-            'bn_chan': 16,
-            'hid_chan': 4,
-            'skip_chan': 8,
-            'n_filters': 32,
+            "n_src": 2,
+            "n_repeats": 2,
+            "n_blocks": 3,
+            "bn_chan": 16,
+            "hid_chan": 4,
+            "skip_chan": 8,
+            "n_filters": 32,
         },
         DPRNNTasNet.__name__: {
-            'n_src': 2,
-            'n_repeats': 2,
-            'bn_chan': 16,
-            'hid_size': 4,
-            'chunk_size': 20,
-            'n_filters': 32,
+            "n_src": 2,
+            "n_repeats": 2,
+            "bn_chan": 16,
+            "hid_size": 4,
+            "chunk_size": 20,
+            "n_filters": 32,
         },
-        DPTNet.__name__: {
-            'n_src': 2,
-            'ff_hid': 10,
-            'chunk_size': 4,
-            'n_repeats': 2,
-        },
+        DPTNet.__name__: {"n_src": 2, "ff_hid": 10, "chunk_size": 4, "n_repeats": 2,},
         LSTMTasNet.__name__: {
-            'n_src': 2,
-            'hid_size': 4,
-            'n_layers': 1,
-            'n_filters': 32,
-            'dropout': 0.0,
+            "n_src": 2,
+            "hid_size": 4,
+            "n_layers": 1,
+            "n_filters": 32,
+            "dropout": 0.0,
         },
         DeMask.__name__: {
-            'input_type': "mag",
-            'output_type': "mag",
-            'hidden_dims': [64],
-            'dropout': 0,
-            'activation': "relu",
-            'mask_act': "relu",
-            'norm_type': "gLN",
-            'n_filters': 8,
-            'stride': 4,
-            'kernel_size': 8
-        }
+            "input_type": "mag",
+            "output_type": "mag",
+            "hidden_dims": [64],
+            "dropout": 0,
+            "activation": "relu",
+            "mask_act": "relu",
+            "norm_type": "gLN",
+            "n_filters": 8,
+            "stride": 4,
+            "kernel_size": 8,
+        },
     }
 
     return params
 
 
 def test_enhancement_model(small_model_params):
-    params = small_model_params['DeMask']
-    filter_banks = ['free', 'stft', 'analytic_free', 'param_sinc']
+    params = small_model_params["DeMask"]
+    filter_banks = ["free", "stft", "analytic_free", "param_sinc"]
     device = get_default_device()
     inputs = ((torch.rand(1, 1000, device=device) - 0.5) * 2,)
     test_data = torch.rand(1, 32, device=device)
@@ -72,16 +67,11 @@ def test_enhancement_model(small_model_params):
             assert torch.allclose(ref, out)
 
 
-@pytest.mark.parametrize('model_def',
-    (
-        ConvTasNet,
-        DPRNNTasNet,
-        DPTNet,
-        LSTMTasNet,
-    )
+@pytest.mark.parametrize(
+    "model_def", (ConvTasNet, DPRNNTasNet, DPTNet, LSTMTasNet,),
 )
 def test_trace_bss_model(small_model_params, model_def):
-    filter_bank_types = ['free', 'stft', 'analytic_free', 'param_sinc']
+    filter_bank_types = ["free", "stft", "analytic_free", "param_sinc"]
     device = get_default_device()
     # Random input uniformly distributed in [-1, 1]
     inputs = ((torch.rand(1, 1000, device=device) - 0.5) * 2,)
@@ -101,5 +91,5 @@ def test_trace_bss_model(small_model_params, model_def):
 
 def get_default_device():
     if torch.cuda.is_available():
-        return 'cuda'
-    return 'cpu'
+        return "cuda"
+    return "cpu"
