52793e8e749f8087bb23ad0834148165c0cd7226
HU Mathieu <mathieu.hu@inria.fr>
2020-09-04 15:38:34 +0200

    Removed tests for script mode, added trace test for ConvTasNet

--- tests/models/jit_test.py
+++ tests/models/jit_test.py
@@ -5,66 +5,48 @@ from asteroid.models import DeMask, ConvTasNet, DPRNNTasNet, DPTNet, LSTMTasNet
 from torch.testing._internal.jit_utils import JitTestCase
 
 
-class TestModels(JitTestCase):
-    @staticmethod
-    def _test_demask(self, device, check_export_import=True):
-        for fb in ["free", "stft", "analytic_free", "param_sinc"]:
-            # set model to eval due to non-deterministic behaviour of dropout
-            model = DeMask(fb_type=fb).eval().to(device)
+@pytest.fixture(scope='module')
+def small_model_params():
+    params = {
+        ConvTasNet.__name__: {
+            'n_src': 2,
+            'n_repeats': 2,
+            'n_blocks': 3,
+            'bn_chan': 16,
+            'hid_chan': 4,
+            'skip_chan': 8,
+            'n_filters': 32,
+        },
+    }
 
-            # test scripting of the separator
-            torch.jit.script(model)
-
-    def test_demask(self):
-        self._test_demask(self, device="cpu")
-
-    @staticmethod
-    def _test_convtasnet(self, device, check_export_import=True):
-        for fb in ["free", "stft", "analytic_free", "param_sinc"]:
-            # set model to eval due to non-deterministic behaviour of dropout
-            model = ConvTasNet(n_src=2, fb_name=fb).eval().to(device)
-
-            # test scripting of the separator
-            torch.jit.script(model)
-
-    def test_convtasnet(self):
-        self._test_convtasnet(self, device="cpu")
+    return params
 
 
-    @staticmethod
-    def _test_drpnntasnet(self, device, check_export_import=True):
-        for fb in ["free", "stft", "analytic_free", "param_sinc"]:
-            # set model to eval due to non-deterministic behaviour of dropout
-            model = DPRNNTasNet(n_src=2, fb_name=fb).eval().to(device)
+@pytest.mark.parametrize('model_def',
+    (
+        ConvTasNet,
+    )
+)
+def test_trace_bss_model(small_model_params, model_def):
+    filter_bank_types = ['free', 'stft', 'analytic_free', 'param_sinc']
+    device = get_default_device()
+    # Random input uniformly distributed in [-1, 1]
+    inputs = ((torch.rand(1, 1000, device=device) - 0.5) * 2,)
+    test_data = torch.rand(1, 32, device=device)
+    for filter_bank in filter_bank_types:
+        params = small_model_params[model_def.__name__]
+        model = model_def(**params, fb_name=filter_bank)
+        model = model.eval().to(device)
+        traced = torch.jit.trace(model, inputs)
 
-            # test scripting of the separator
-            torch.jit.script(model)
-
-    def test_drpnntasnet(self):
-        self._test_drpnntasnet(self, device="cpu")
-
-
-    @staticmethod
-    def _test_dptnet(self, device, check_export_import=True):
-        for fb in ["free", "stft", "analytic_free", "param_sinc"]:
-            # set model to eval due to non-deterministic behaviour of dropout
-            model = DPTNet(n_src=2, fb_name=fb).eval().to(device)
-
-            # test scripting of the separator
-            torch.jit.script(model)
-
-    def test_dptnet(self):
-        self._test_dptnet(self, device="cpu")
+        # check forward
+        with torch.no_grad():
+            ref = model(test_data)
+            out = traced(test_data)
+            assert torch.allclose(ref, out)
 
 
-    @staticmethod
-    def _test_lstmtasnet(self, device, check_export_import=True):
-        for fb in ["free", "stft", "analytic_free", "param_sinc"]:
-            # set model to eval due to non-deterministic behaviour of dropout
-            model = LSTMTasNet(n_src=2, fb_name=fb).eval().to(device)
-
-            # test scripting of the separator
-            torch.jit.script(model)
-
-    def test_lstmtasnet(self):
-        self._test_lstmtasnet(self, device="cpu")
+def get_default_device():
+    if torch.cuda.is_available():
+        return 'cuda'
+    return 'cpu'
