5d7c254dfa4a82a542e289530da9318d68936905
mpariente <pariente.mnl@gmail.com>
2020-10-24 22:01:44 +0200

    For script_if_tracing from torch's master for 1.6.0 compat

--- asteroid/utils/torch_utils.py
+++ asteroid/utils/torch_utils.py
@@ -1,3 +1,5 @@
+import functools
+
 import torch
 from torch import nn
 from collections import OrderedDict
@@ -126,9 +128,44 @@ def are_models_equal(model1, model2):
     return True
 
 
-# Import `scipt_if_tracing` if available
-script_if_tracing = getattr(
-    torch.jit,
-    "script_if_tracing",
-    getattr(torch.jit, "_script_if_tracing", torch.jit.script),
-)
+def is_tracing():
+    # Taken for pytorch for compat in 1.6.0
+    """
+    Returns ``True`` in tracing (if a function is called during the tracing of
+    code with ``torch.jit.trace``) and ``False`` otherwise.
+    """
+    return torch._C._is_tracing()
+
+
+def script_if_tracing(fn):
+    # Taken for pytorch for compat in 1.6.0
+    """
+    Compiles ``fn`` when it is first called during tracing. ``torch.jit.script``
+    has a non-negligible start up time when it is first called due to
+    lazy-initializations of many compiler builtins. Therefore you should not use
+    it in library code. However, you may want to have parts of your library work
+    in tracing even if they use control flow. In these cases, you should use
+    ``@torch.jit.script_if_tracing`` to substitute for
+    ``torch.jit.script``.
+
+    Arguments:
+        fn: A function to compile.
+
+    Returns:
+        If called during tracing, a :class:`ScriptFunction` created by `torch.jit.script` is returned.
+        Otherwise, the original function `fn` is returned.
+    """
+
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        if not is_tracing():
+            # Not tracing, don't do anything
+            return fn(*args, **kwargs)
+
+        compiled_fn = torch.jit.script(wrapper.__original_fn)  # type: ignore
+        return compiled_fn(*args, **kwargs)
+
+    wrapper.__original_fn = fn  # type: ignore
+    wrapper.__script_if_tracing_wrapper = True  # type: ignore
+
+    return wrapper
