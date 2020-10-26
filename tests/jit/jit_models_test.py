411dafb5f9c71f5eb9a2d24174cd109a8695b37c
mpariente <pariente.mnl@gmail.com>
2020-10-26 13:54:35 +0100

    SUDORMRF JIt tests

--- tests/jit/jit_models_test.py
+++ tests/jit/jit_models_test.py
@@ -1,7 +1,15 @@
 import torch
 import pytest
 from torch.testing import assert_allclose
-from asteroid.models import DeMask, ConvTasNet, DPRNNTasNet, DPTNet, LSTMTasNet
+from asteroid.models import (
+    DeMask,
+    ConvTasNet,
+    DPRNNTasNet,
+    DPTNet,
+    LSTMTasNet,
+    SuDORMRFNet,
+    SuDORMRFImprovedNet,
+)
 
 
 @pytest.fixture(scope="module")
@@ -59,6 +67,24 @@ def small_model_params():
             "n_filters": 32,
             "kernel_size": 32,
         },
+        SuDORMRFNet.__name__: {
+            "n_src": 2,
+            "bn_chan": 10,
+            "num_blocks": 2,
+            "upsampling_depth": 2,
+            "n_filters": 32,
+            "kernel_size": 21,
+            "stride": 10,
+        },
+        SuDORMRFImprovedNet.__name__: {
+            "n_src": 2,
+            "bn_chan": 10,
+            "num_blocks": 2,
+            "upsampling_depth": 2,
+            "n_filters": 32,
+            "kernel_size": 21,
+            "stride": 10,
+        },
     }
 
     return params
@@ -75,7 +101,7 @@ def test_enhancement_model(small_model_params, test_data):
     params = small_model_params["DeMask"]
     filter_bank = "free"
     device = get_default_device()
-    inputs = ((torch.rand(1, 200, device=device) - 0.5) * 2,)
+    inputs = ((torch.rand(1, 201, device=device) - 0.5) * 2,)
     test_data = test_data.to(device)
     model = DeMask(**params, fb_type=filter_bank).eval().to(device)
     traced = torch.jit.trace(model, inputs)
@@ -94,6 +120,8 @@ def test_enhancement_model(small_model_params, test_data):
         DPRNNTasNet,
         DPTNet,
         LSTMTasNet,
+        SuDORMRFNet,
+        SuDORMRFImprovedNet,
     ),
 )
 @pytest.mark.parametrize(
@@ -102,14 +130,14 @@ def test_enhancement_model(small_model_params, test_data):
         (torch.rand(240) - 0.5) * 2,
         (torch.rand(1, 220) - 0.5) * 2,
         (torch.rand(3, 250) - 0.5) * 2,
-        (torch.rand(1, 1, 300) - 0.5) * 2,
+        (torch.rand(1, 1, 301) - 0.5) * 2,
     ),
 )
 def test_trace_bss_model(small_model_params, model_def, test_data):
     filter_bank_type = "free"
     device = get_default_device()
     # Random input uniformly distributed in [-1, 1]
-    inputs = ((torch.rand(1, 200, device=device) - 0.5) * 2,)
+    inputs = ((torch.rand(1, 201, device=device) - 0.5) * 2,)
     test_data = test_data.to(device)
     params = small_model_params[model_def.__name__]
     model = model_def(**params, fb_name=filter_bank_type)
@@ -127,3 +155,34 @@ def get_default_device():
     if torch.cuda.is_available():
         return "cuda"
     return "cpu"
+
+
+# def test_padder():
+#     model_def = SuDORMRFNet
+#     params = small_model_params[model_def.__name__]
+#     model = model_def(**params, fb_name="free")
+
+
+if __name__ == "__main__":
+    model_def = SuDORMRFNet
+    # params = small_model_params[model_def.__name__]
+    params = {
+        "stride": 10,
+        "n_src": 2,
+        "bn_chan": 10,
+        "num_blocks": 2,
+        "upsampling_depth": 2,
+        "n_filters": 32,
+        "kernel_size": 21,
+    }
+    model = model_def(**params, fb_name="free")
+    u = torch.randn(3, 250)
+    import ipdb
+
+    ipdb.set_trace()
+    v = model(u)
+
+    traced = torch.jit.trace(model, torch.randn(1, 210))
+    hey = traced(u)
+
+    assert_allclose(v, hey)
