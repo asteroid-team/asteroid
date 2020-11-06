9da72b48ab7bf3d094134bd55fad6f188cfc24e6
Gerardo Roa Dabike <gerardo.roa@gmail.com>
2020-11-06 14:25:04 +0000

    Reduce the duration decimal places.
    Signed-off-by: gerardo <gerardo.roa@gmail.com>

--- asteroid/data/dampvsep_dataset.py
+++ asteroid/data/dampvsep_dataset.py
@@ -121,6 +121,8 @@ class DAMPVSEPSinglesDataset(torch.utils.data.Dataset):
         return x
 
     def __getitem__(self, index):
+        import math
+
         audio_sources = {}
         track_id = index // self.ex_per_track
         perf = self.perf_key[track_id]
@@ -129,7 +131,7 @@ class DAMPVSEPSinglesDataset(torch.utils.data.Dataset):
 
         # Set start time of segment
         start = 0.0
-        duration = float(self.tracks[perf]["duration"])
+        duration = math.floor(float(self.tracks[perf]["duration"]) * 100) / 100
         if self.random_segments:
             start = random.uniform(0.0, float(self.tracks[perf]["duration"]) - self.segment)
             duration = float(self.segment)
