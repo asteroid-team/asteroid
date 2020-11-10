63fd7b13eb29825782729425d73498b85e4cdd17
popcornell <cornellsamuele@gmail.com>
2020-11-10 22:53:41 +0100

    moving all schedulers to lightning, using step from now on.

--- asteroid/engine/system.py
+++ asteroid/engine/system.py
@@ -116,15 +116,6 @@ class System(pl.LightningModule):
         tensorboard_logs = {"train_loss": loss}
         return {"loss": loss, "log": tensorboard_logs}
 
-    def optimizer_step(self, *args, **kwargs) -> None:
-        if self.scheduler is not None:
-            if not isinstance(self.scheduler, (list, tuple)):
-                self.scheduler = [self.scheduler]  # support multiple schedulers
-            for sched in self.scheduler:
-                if isinstance(sched, dict) and sched["interval"] == "batch":
-                    sched["scheduler"].step()  # call step on each batch scheduler
-        super().optimizer_step(*args, **kwargs)
-
     def validation_step(self, batch, batch_nb):
         """Need to overwrite PL validation_step to do validation.
 
@@ -176,8 +167,8 @@ class System(pl.LightningModule):
             else:
                 assert sched["interval"] in [
                     "batch",
-                    "epoch",
-                ], "Scheduler interval should be either batch or epoch"
+                    "step",
+                ], "Scheduler interval should be either step or epoch"
                 epoch_schedulers.append(sched)
         return [self.optimizer], epoch_schedulers
 
