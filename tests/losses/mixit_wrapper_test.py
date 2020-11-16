1247b75e210ba88587204488749e01522bb7a35b
mpariente <pariente.mnl@gmail.com>
2020-11-16 14:45:53 +0100

    Generalized as default

--- tests/losses/mixit_wrapper_test.py
+++ tests/losses/mixit_wrapper_test.py
@@ -17,7 +17,7 @@ def test_mixitwrapper_as_pit_wrapper(batch_size, n_src, time):
     est_targets = torch.randn(batch_size, n_src, time)
 
     # mix_it base case: targets == mixtures / With and without return estimates
-    loss = MixITLossWrapper(good_batch_loss_func)
+    loss = MixITLossWrapper(good_batch_loss_func, generalized=False)
     loss(est_targets, targets)
     loss_value, reordered_est = loss(est_targets, targets, return_est=True)
     assert reordered_est.shape == est_targets.shape
@@ -33,14 +33,14 @@ def test_mixit_wrapper(batch_size, factor, n_mix, time):
     est_targets = torch.randn(batch_size, n_src, time)
 
     # mix_it / With and without return estimates
-    loss = MixITLossWrapper(good_batch_loss_func)
+    loss = MixITLossWrapper(good_batch_loss_func, generalized=False)
     loss(est_targets, mixtures)
     loss_value, reordered_mix = loss(est_targets, mixtures, return_est=True)
     assert reordered_mix.shape == mixtures.shape
 
 
 @pytest.mark.parametrize("batch_size", [1, 2, 8])
-@pytest.mark.parametrize("n_src", [2, 3, 4])
+@pytest.mark.parametrize("n_src", [2, 3, 4, 5])
 @pytest.mark.parametrize("n_mix", [2])
 @pytest.mark.parametrize("time", [16000])
 def test_mixit_gen_wrapper(batch_size, n_src, n_mix, time):
@@ -48,7 +48,7 @@ def test_mixit_gen_wrapper(batch_size, n_src, n_mix, time):
     est_targets = torch.randn(batch_size, n_src, time)
 
     # mix_it_gen / With and without return estimates. Works only with two mixtures
-    loss = MixITLossWrapper(good_batch_loss_func, generalized=True)
+    loss = MixITLossWrapper(good_batch_loss_func)
     loss(est_targets, mixtures)
     loss_value, reordered_est = loss(est_targets, mixtures, return_est=True)
     assert reordered_est.shape == mixtures.shape
