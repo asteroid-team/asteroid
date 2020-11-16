f97974605db6a287f896f1e66ac7e5e17338aea0
mpariente <pariente.mnl@gmail.com>
2020-11-16 23:13:07 +0100

    get_text.py template

---
+++ egs/librimix/ConvTasNet/local/get_text.py
@@ -0,0 +1,28 @@
+import argparse
+import glob
+import os
+import pandas as pd
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--libridir", required=True, type=str)
+parser.add_argument("--outdir", required=True, type=str)
+parser.add_argument("--split", type=str, default="train-360")
+
+args = parser.parse_args()
+
+libridir = f"LibriSpeech/{args.split}"
+trans_txt_list = glob.glob(os.path.join(libridir, "**/*.txt"), recursive=True)
+
+row_list = []
+for name in trans_txt_list:
+    f = open(name, "r")
+    for line in f:
+        dict1 = {}
+        split_line = line.split(" ", maxsplit=1)
+        dict1["utt_id"] = split_line[0]
+        dict1["text"] = split_line[1].replace("\n", "").replace("\r", "")
+        row_list.append(dict1)
+
+df = pd.DataFrame(row_list)
+df.to_csv(os.path.join(args.outdir, "annotations.csv"), index=False)
