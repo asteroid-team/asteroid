27cb6aef5befab7b0eb659aae113adb48de842ab
mpariente <pariente.mnl@gmail.com>
2020-11-16 23:10:09 +0100

    Retrieve transcriptions in prepare_data.sh

--- egs/librimix/ConvTasNet/local/prepare_data.sh
+++ egs/librimix/ConvTasNet/local/prepare_data.sh
@@ -17,3 +17,9 @@ cd LibriMix
 cd $current_dir
 $python_path local/create_local_metadata.py --librimix_dir $storage_dir/Libri$n_src"Mix"
 
+
+# TODO: check folders and outdir
+#dev-clean  test-clean  train-clean-100  train-clean-360
+for split in train-360 train-100 dev-clean test-clean; do
+  $python_path local/get_text.py --libridir $storage_dir/LibriSpeech --split $split --outdir data/$split
+done
