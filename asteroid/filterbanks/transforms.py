2e0fe13e31b6c219e56290aedefa8ec9f2063ee3
mpariente <pariente.mnl@gmail.com>
2020-11-18 20:13:27 +0100

    Add features delta computation

--- asteroid/filterbanks/transforms.py
+++ asteroid/filterbanks/transforms.py
@@ -376,3 +376,45 @@ def ebased_vad(mag_spec, th_db: int = 40):
     to_view = list(mag_spec.shape[:-2]) + [1, -1]
     max_log_mag = torch.max(log_mag.view(to_view), -1, keepdim=True)[0]
     return log_mag > (max_log_mag - th_db)
+
+
+def compute_delta(feats: torch.Tensor, dim: int = -1) -> torch.Tensor:
+    """Compute delta coefficients of a tensor.
+
+    Args:
+        feats: Input features to compute deltas with.
+        dim: feature dimension in the feats tensor.
+
+    Returns:
+        Tensor: Tensor of deltas.
+
+    Examples
+        TODO
+    """
+    if dim != -1:
+        return compute_delta(feats.transpose(-1, dim), dim=-1).transpose(-1, dim)
+    # First frame has nothing. Then each frame is the diff with the previous one.
+    delta = feats.new_zeros(feats.shape)
+    delta[..., 1:] = feats[..., 1:] - feats[..., :-1]
+    return delta
+
+
+def concat_deltas(feats: torch.Tensor, order: int = 1, dim: int = -1) -> torch.Tensor:
+    """Concatenate delta coefficients of a tensor to itself.
+
+    Args:
+        feats: Input features to compute deltas with.
+        order: Order of the delta e.g with order==2, compute delta of delta
+            as well.
+        dim: feature dimension in the feats tensor.
+
+    Returns:
+        Tensor: Concatenation of the features, the deltas and subsequent deltas.
+
+    Examples
+        TODO
+    """
+    all_feats = [feats]
+    for _ in range(order):
+        all_feats.append(compute_delta(feats[-1], dim=dim))
+    return torch.cat(all_feats, dim=dim)
