487a7a8d6a6558aec15f4db09d46b76ccfb569fc
Pariente Manuel <pariente.mnl@gmail.com>
2020-11-19 18:40:34 +0100

    [src] BaseModel takes sample_rate argument (#336)
    Addresses #318 by adding an optional sample_rate argument to BaseModel with a property getter and setter.

--- asteroid/models/base_models.py
+++ asteroid/models/base_models.py
@@ -32,8 +32,9 @@ class BaseModel(nn.Module):
 
     """
 
-    def __init__(self):
+    def __init__(self, sample_rate: float = 8000.0):
         super().__init__()
+        self.__sample_rate = sample_rate
 
     def forward(self, *args, **kwargs):
         raise NotImplementedError
@@ -41,7 +42,16 @@ class BaseModel(nn.Module):
     @property
     def sample_rate(self):
         """Operating sample rate of the model (float)."""
-        raise NotImplementedError
+        return self.__sample_rate
+
+    @sample_rate.setter
+    def sample_rate(self, new_sample_rate: float):
+        warnings.warn(
+            "Other sub-components of the model might have a `sample_rate` "
+            "attribute, be sure to modify them for consistency.",
+            UserWarning,
+        )
+        self.__sample_rate = new_sample_rate
 
     @torch.no_grad()
     def separate(self, wav, output_dir=None, force_overwrite=False, resample=False, **kwargs):
@@ -275,16 +285,12 @@ class BaseEncoderMaskerDecoder(BaseModel):
     """
 
     def __init__(self, encoder, masker, decoder, encoder_activation=None):
-        super().__init__()
+        super().__init__(sample_rate=getattr(encoder, "sample_rate", None))
         self.encoder = encoder
         self.masker = masker
         self.decoder = decoder
         self.encoder_activation = encoder_activation
         self.enc_activation = activations.get(encoder_activation or "linear")()
-
-    @property
-    def sample_rate(self):
-        return getattr(self.encoder, "sample_rate", None)
 
     def forward(self, wav):
         """Enc/Mask/Dec model forward
