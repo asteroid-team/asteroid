a43a56d0b914ca82218f3411276f30260f8b9812
mpariente <pariente.mnl@gmail.com>
2020-11-20 20:37:32 +0100

    Separate DCUNet's test + meaningful tests

--- tests/jit/jit_models_test.py
+++ tests/jit/jit_models_test.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import torch
 import pytest
 from torch.testing import assert_allclose
@@ -12,6 +14,13 @@ from asteroid.models import (
     SuDORMRFNet,
     SuDORMRFImprovedNet,
 )
+
+
+@torch.no_grad()
+def assert_consistency(model, traced, tensor):
+    ref = model(tensor)
+    out = traced(tensor)
+    assert_allclose(ref, out)
 
 
 @pytest.fixture(scope="module")
@@ -49,10 +58,8 @@ def small_model_params():
             "stride": 16,
         },
         DCCRNet.__name__: {
+            "stft_kernel_size": 256,
             "architecture": "DCCRN-CL",
-        },
-        DCUNet.__name__: {
-            "architecture": "DCUNet-10",
         },
         DeMask.__name__: {
             "input_type": "mag",
@@ -94,26 +101,18 @@ def small_model_params():
             "stride": 10,
         },
     }
-
     return params
 
 
-@pytest.mark.parametrize(
-    "model_def",
-    (
-        DCCRNet,
-        DCUNet,
-        DeMask,
-    ),
-)
+@pytest.mark.parametrize("model_def", [DCCRNet, DeMask])
 @pytest.mark.parametrize(
     "test_data",
     (
-        (torch.rand(4740) - 0.5) * 2,
+        (torch.rand(2001) - 0.5) * 2,
         (torch.rand(1, 4720) - 0.5) * 2,
-        (torch.rand(4, 4756) - 0.5) * 2,
-        (torch.rand(1, 1, 4801) - 0.5) * 2,
-        (torch.rand(3, 1, 4790) - 0.5) * 2,
+        (torch.rand(4, 1100) - 0.5) * 2,
+        (torch.rand(1, 1, 1502) - 0.5) * 2,
+        (torch.rand(3, 1, 4301) - 0.5) * 2,
     ),
 )
 def test_enhancement_model(small_model_params, model_def, test_data):
@@ -122,15 +121,24 @@ def test_enhancement_model(small_model_params, model_def, test_data):
     model = model_def(**params)
     model = model.eval().to(device)
     # Random input uniformly distributed in [-1, 1]
-    inputs = ((torch.rand(1, 4701, device=device) - 0.5) * 2,)
+    inputs = ((torch.rand(1, 2500, device=device) - 0.5) * 2,)
     traced = torch.jit.trace(model, inputs)
 
-    # check forward
-    test_data = test_data.to(device)
-    with torch.no_grad():
-        ref = model(test_data)
-        out = traced(test_data)
-        assert_allclose(ref, out)
+    assert_consistency(model=model, traced=traced, tensor=test_data.to(device))
+
+
+@pytest.mark.parametrize("test_shape", [(2,), (3, 1)])
+@pytest.mark.parametrize("matching_samples", [4701, 8800, 17001])
+def test_dcunet_model(test_shape: Tuple, matching_samples):
+    n_samples = 4701
+    device = get_default_device()
+    model = DCUNet(architecture="DCUNet-10").eval().to(device)
+    # Random input uniformly distributed in [-1, 1]
+    inputs = torch.rand(1, n_samples, device=device)
+    traced = torch.jit.trace(model, (inputs,))
+
+    test_data = torch.rand(*test_shape, matching_samples, device=device)
+    assert_consistency(model=model, traced=traced, tensor=test_data.to(device))
 
 
 @pytest.mark.parametrize(
@@ -151,6 +159,7 @@ def test_enhancement_model(small_model_params, model_def, test_data):
         (torch.rand(1, 220) - 0.5) * 2,
         (torch.rand(3, 250) - 0.5) * 2,
         (torch.rand(1, 1, 301) - 0.5) * 2,
+        (torch.rand(2, 1, 501) - 0.5) * 2,
     ),
 )
 def test_trace_bss_model(small_model_params, model_def, test_data):
@@ -162,46 +171,10 @@ def test_trace_bss_model(small_model_params, model_def, test_data):
     inputs = ((torch.rand(1, 201, device=device) - 0.5) * 2,)
     traced = torch.jit.trace(model, inputs)
 
-    # check forward
-    test_data = test_data.to(device)
-    with torch.no_grad():
-        ref = model(test_data)
-        out = traced(test_data)
-        assert_allclose(ref, out)
+    assert_consistency(model=model, traced=traced, tensor=test_data.to(device))
 
 
 def get_default_device():
     if torch.cuda.is_available():
         return "cuda"
     return "cpu"
-
-
-# def test_padder():
-#     model_def = SuDORMRFNet
-#     params = small_model_params[model_def.__name__]
-#     model = model_def(**params)
-
-
-if __name__ == "__main__":
-    model_def = SuDORMRFNet
-    # params = small_model_params[model_def.__name__]
-    params = {
-        "stride": 10,
-        "n_src": 2,
-        "bn_chan": 10,
-        "num_blocks": 2,
-        "upsampling_depth": 2,
-        "n_filters": 32,
-        "kernel_size": 21,
-    }
-    model = model_def(**params)
-    u = torch.randn(3, 250)
-    import ipdb
-
-    ipdb.set_trace()
-    v = model(u)
-
-    traced = torch.jit.trace(model, torch.randn(1, 210))
-    hey = traced(u)
-
-    assert_allclose(v, hey)
