ea6b58058b12ef510d2f85b1b7d9d0b1286773c1
mpariente <pariente.mnl@gmail.com>
2020-11-25 21:14:42 +0100

    Add help for now

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,4 @@
+import subprocess
 from asteroid.scripts import asteroid_versions
 
 
@@ -15,3 +16,20 @@ def test_print_versions():
 def test_asteroid_versions_without_git(monkeypatch):
     monkeypatch.setenv("PATH", "")
     asteroid_versions.asteroid_versions()
+
+
+def cmd(cmd_str):
+    str_list = cmd_str.split(" ")
+    return subprocess.check_output(str_list).strip().decode("ascii", "ignore")
+
+
+def test_register_sr():
+    cmd("asteroid-register-sr --help")
+
+
+def test_infer():
+    cmd("asteroid-infer ")
+
+
+def test_upload():
+    cmd("asteroid-upload --help")
