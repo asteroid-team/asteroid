7dbc6e121721c11e1e0253cd13355823b23b7374
mpariente <pariente.mnl@gmail.com>
2020-11-28 20:38:48 +0100

    Order all by source

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -385,7 +385,8 @@ autodoc_mock_imports = MOCK_REQUIRE_PACKAGES + MOCK_MANUAL_PACKAGES
 autodoc_inherit_docstring = False
 autodoc_default_flags = ["members", "show-inheritance"]
 # Order functions by appearance in source (default 'alphabetical')
-autodoc_member_order = "groupwise"
+autodoc_member_order = "bysource"
+# autodoc_member_order = "groupwise"
 
 
 # autodoc_member_order = 'groupwise'
