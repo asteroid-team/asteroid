1c81542548d2c3fa80c20fba7d8c83fb58f39e47
Julien Chaumond <chaumond@gmail.com>
2020-12-04 23:26:06 +0100

    Update hub_utils_test.py

--- tests/utils/hub_utils_test.py
+++ tests/utils/hub_utils_test.py
@@ -2,10 +2,22 @@ import os
 from asteroid.utils import hub_utils
 
 
+HF_EXAMPLE_MODEL_IDENTIFER = "julien-c/DPRNNTasNet-ks16_WHAM_sepclean"
+
+
 def test_download():
     # We download
     path1 = hub_utils.cached_download("mpariente/ConvTasNet_WHAM!_sepclean")
     assert os.path.isfile(path1)
     # We use cache
     path2 = hub_utils.cached_download("mpariente/ConvTasNet_WHAM!_sepclean")
+    assert path1 == path2
+
+
+def test_hf_download():
+    # We download
+    path1 = hub_utils.cached_download(HF_EXAMPLE_MODEL_IDENTIFER)
+    assert os.path.isfile(path1)
+    # We use cache
+    path2 = hub_utils.cached_download(HF_EXAMPLE_MODEL_IDENTIFER)
     assert path1 == path2
