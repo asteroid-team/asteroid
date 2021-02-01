8abe8447a664ed64265979efd96feb976c881337
Xuechen Liu <xuliu@grue-3.nancy.grid5000.fr>
2021-02-01 22:40:00 +0100

    delete...

--- egs/wham/ConvTasNet/local/prepare_data.sh
+++ egs/wham/ConvTasNet/local/prepare_data.sh
@@ -15,16 +15,18 @@ if [ ! -d $out_dir/wham_noise ]; then
 	unzip $out_dir/wham_noise.zip -d $out_dir >> $out_dir/logs/unzip_wham.log
 fi
 
-# echo "Download WHAM scripts into $out_dir"
-# wget https://storage.googleapis.com/whisper-public/wham_scripts.tar.gz -P $out_dir
-# tar -xzvf $out_dir/wham_scripts.tar.gz -C $out_dir
-# mv $out_dir/wham_scripts.tar.gz $out_dir/wham_scripts
+echo "Download WHAM scripts into $out_dir"
+wget https://storage.googleapis.com/whisper-public/wham_scripts.tar.gz -P $out_dir
+tar -xzvf $out_dir/wham_scripts.tar.gz -C $out_dir
+mv $out_dir/wham_scripts.tar.gz $out_dir/wham_scripts
 
-# wait
+wait
 
 echo "Run python scripts to create the WHAM mixtures"
 # Requires : Numpy, Scipy, Pandas, and Pysoundfile
-python3 $out_dir/create_wham_from_scratch.py \
+cd $out_dir/wham_scripts
+python3 create_wham_from_scratch.py \
 	--wsj0-root $wav_dir \
 	--wham-noise-root $out_dir/wham_noise\
 	--output-dir $out_dir
+cd -
