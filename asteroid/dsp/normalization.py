cf6d73714fc18c6216c2596cde6cbe7c1a5b6024
JorisCos <joris.cosentino.pro@gmail.com>
2021-02-02 15:20:39 +0100

    black normalization.py add test normalization_test.py

--- asteroid/dsp/normalization.py
+++ asteroid/dsp/normalization.py
@@ -2,14 +2,13 @@ import numpy as np
 
 
 def normalize_estimates(est_np, mix_np):
-    """ Normalizes estimates according to the mixture maximum amplitude
+    """Normalizes estimates according to the mixture maximum amplitude
 
     Args:
         est_np (numpy array): Estimates (nb_estimates, time)
-        mix_np (numpy array): One mixture
+        mix_np (numpy array): One mixture (time)
 
 
     """
     mix_max = np.max(np.abs(mix_np))
-    return np.stack([est * mix_max / np.max(np.abs(est)) for est in est_np],
-                    dim=0)
+    return np.stack([est * mix_max / np.max(np.abs(est)) for est in est_np])
