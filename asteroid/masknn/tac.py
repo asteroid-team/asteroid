dc9bc5c44c12e9516b7dc74aad2c0fa2f9a105b0
Manuel Pariente <pariente.mnl@gmail.com>
2021-02-08 16:25:11 +0100

    Edit tac.py again: caps

--- asteroid/masknn/tac.py
+++ asteroid/masknn/tac.py
@@ -4,17 +4,17 @@ from . import activations, norms
 
 
 class TAC(nn.Module):
-    """Transform-Average-Concatenate inter-microphone-channel permutation invariant communication block [1]
-
-    .. note:: Supports inputs of shape :math:`(batch, mic\_channels, features, chunk\_size, n\_chunks)`
-        as in FasNet-TAC. The operations are applied for each element in ``chunk_size`` and ``n_chunks``.
-        Output is of same shape as input.
+    """Transform-Average-Concatenate inter-microphone-channel permutation invariant communication block [1].
 
     Args:
         input_dim (int): Number of features of input representation.
         hidden_dim (int, optional): size of hidden layers in TAC operations.
         activation (str, optional): type of activation used. See asteroid.masknn.activations.
         norm_type (str, optional): type of normalization layer used. See asteroid.masknn.norms.
+
+    .. note:: Supports inputs of shape :math:`(batch, mic\_channels, features, chunk\_size, n\_chunks)`
+        as in FasNet-TAC. The operations are applied for each element in ``chunk_size`` and ``n_chunks``.
+        Output is of same shape as input.
 
     References
         [1] : Luo, Yi, et al. "End-to-end microphone permutation and number invariant multi-channel
@@ -50,25 +50,25 @@ class TAC(nn.Module):
             output (:class:`torch.Tensor`): features for each mic_channel after TAC inter-channel processing.
                 Shape :math:`(batch, mic\_channels, features, chunk\_size, n\_chunks)`.
         """
-        # input is 5D because it is multi-channel DPRNN. DPRNN single channel is 4D.
+        # Input is 5D because it is multi-channel DPRNN. DPRNN single channel is 4D.
         batch_size, nmics, channels, chunk_size, n_chunks = x.size()
-        # first operation: transform the input for each frame and independently on each mic channel.
+        # First operation: transform the input for each frame and independently on each mic channel.
         output = self.input_tf(
             x.permute(0, 3, 4, 1, 2).reshape(batch_size * nmics * chunk_size * n_chunks, channels)
         ).reshape(batch_size, chunk_size, n_chunks, nmics, self.hidden_dim)
 
-        # mean pooling across channels
+        # Mean pooling across channels
         if valid_mics.max() == 0:
-            # fixed geometry array
+            # Fixed geometry array
             mics_mean = output.mean(1)
         else:
-            # only consider valid channels in each batch element: each example can have different number of microphones.
+            # Only consider valid channels in each batch element: each example can have different number of microphones.
             mics_mean = [
                 output[b, :, :, : valid_mics[b]].mean(2).unsqueeze(0) for b in range(batch_size)
             ]  # 1, dim1*dim2, H
             mics_mean = torch.cat(mics_mean, 0)  # B*dim1*dim2, H
 
-        # the average is processed by a non-linear transform
+        # The average is processed by a non-linear transform
         mics_mean = self.avg_tf(
             mics_mean.reshape(batch_size * chunk_size * n_chunks, self.hidden_dim)
         )
@@ -78,7 +78,8 @@ class TAC(nn.Module):
             .expand_as(output)
         )
 
-        # concatenate the transformed average in each channel with the original feats and project back to same number of features
+        # Concatenate the transformed average in each channel with the original feats and
+        # project back to same number of features
         output = torch.cat([output, mics_mean], -1)
         output = self.concat_tf(
             output.reshape(batch_size * chunk_size * n_chunks * nmics, -1)
