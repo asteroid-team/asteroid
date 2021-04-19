155847e777886237a7d3a89fec28acb0877aa85a
Manuel Pariente <pariente.mnl@gmail.com>
2021-04-19 14:04:21 +0200

    Add global imports

--- asteroid/dsp/__init__.py
+++ asteroid/dsp/__init__.py
@@ -1,5 +1,13 @@
 from .consistency import mixture_consistency
 from .overlap_add import LambdaOverlapAdd, DualPathProcessing
+from .beamforming import (
+    SCM,
+    Beamformer,
+    RTFMVDRBeamformer,
+    SoudenMVDRBeamformer,
+    SDWMWFBeamformer,
+    GEVBeamformer,
+)
 
 __all__ = [
     "mixture_consistency",
