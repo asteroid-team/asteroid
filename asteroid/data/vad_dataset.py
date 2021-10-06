11ce0457ad93edf6ab0febb8206cfa2cf5507906
JorisCos <joris.cosentino.pro@gmail.com>
2021-10-06 17:15:50 +0200

    fix dataset

--- asteroid/data/vad_dataset.py
+++ asteroid/data/vad_dataset.py
@@ -12,12 +12,13 @@ class VADDataset(Dataset):
         md_file_path (str): The path to the metadata file.
     """
 
-    def __init__(self, md_file_path, segment=3):
+    def __init__(self, md_file_path, sample_rate=8000, segment=3):
 
         self.md_filepath = md_file_path
         with open(self.md_filepath) as json_file:
             self.md = json.load(json_file)
         self.segment = segment
+        self.sample_rate = sample_rate
 
     def __len__(self):
         return len(self.md)
@@ -29,8 +30,8 @@ class VADDataset(Dataset):
         self.source_path = row[f"mixture_path"]
         length = len(sf.read(self.source_path)[0])
         if self.segment is not None:
-            start = random.randint(0, length - int(self.segment * 8000))
-            stop = start + int(self.segment * 8000)
+            start = random.randint(0, length - int(self.segment * self.sample_rate))
+            stop = start + int(self.segment * self.sample_rate)
         else:
             start = 0
             stop = None
