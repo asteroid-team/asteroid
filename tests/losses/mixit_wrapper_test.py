68fc6a21013b8fc57a85731f3bb704dbef232785
Zmolikova Katerina <izmolikova@pcspeech4.fit.vutbr.cz>
2022-02-09 10:56:33 +0100

    [tests] Add test for MixIT loss dimension check

--- tests/losses/mixit_wrapper_test.py
+++ tests/losses/mixit_wrapper_test.py
@@ -2,6 +2,7 @@ import pytest
 import torch
 
 from asteroid.losses import MixITLossWrapper
+from asteroid.losses import pairwise_neg_sisdr, multisrc_neg_sisdr
 
 
 def good_batch_loss_func(y_pred, y_true):
@@ -52,3 +53,23 @@ def test_mixit_gen_wrapper(batch_size, n_src, n_mix, time):
     loss(est_targets, mixtures)
     loss_value, reordered_est = loss(est_targets, mixtures, return_est=True)
     assert reordered_est.shape == mixtures.shape
+
+
+@pytest.mark.parametrize("batch_size", [1, 8])
+@pytest.mark.parametrize("factor", [1, 2, 3])
+@pytest.mark.parametrize("n_mix", [2])
+@pytest.mark.parametrize("time", [16000])
+@pytest.mark.parametrize("generalized", [True, False])
+def test_mixitwrapper_checks_loss_shape(batch_size, factor, n_mix, time, generalized):
+    mixtures = torch.randn(batch_size, n_mix, time)
+    n_src = n_mix * factor
+    est_targets = torch.randn(batch_size, n_src, time)
+
+    # correct usage
+    loss = MixITLossWrapper(multisrc_neg_sisdr, generalized=generalized)
+    loss(est_targets, mixtures)
+
+    # incorrect usage
+    loss = MixITLossWrapper(pairwise_neg_sisdr, generalized=generalized)
+    with pytest.raises(ValueError):
+        loss(est_targets, mixtures)
