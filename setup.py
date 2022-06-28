5d4498e3eb4615b8f5442efa4c43530ce77906ea
Manuel Pariente <pariente.mnl@gmail.com>
2022-06-28 17:01:56 +0200

    Update tests to python version to 3.8

--- setup.py
+++ setup.py
@@ -67,10 +67,9 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
